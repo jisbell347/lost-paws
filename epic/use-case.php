3a525c8eba5f4660f12067b211f353c57c4db25e
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-25 16:52:41 -0600

    change reunited to found.

--- epic/use-case.php
+++ epic/use-case.php
@@ -30,7 +30,7 @@
 			<li>Walt sets up a user account by signing in with his google id, then provides LostPaws with the following information about his lost dog: Name, Chip#, Breed, Gender, Color, Size, and other distinguishing features.</li>
 			<li>After submitting the lost dog posting, Walt is given the option to print out an automatically generated lost dog poster. Walt prints 80 copies and plasters every telephone pole in his neighborhood.</li>
 			<li>Three weeks later Walt receives a phone call from a young college student named Margo. Margo found walt's dog in the Bosque. Walter arranges to meet with Margo the same day at the local police station to be reunited with his beloved heeler mix.</li>
-			<li>Walt updates his lost pet listing to "reunited."</li>
+			<li>Walt updates his lost pet listing to "found."</li>
 		</ol>
 
 
