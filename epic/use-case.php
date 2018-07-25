7f627aa431f41bc4ec08c1874b0776b043dafdf8
Adel Moreno <adel.argenix@gmail.com>
2018-07-25 15:36:38 -0600

    added use case for Kristi Wilkins

--- epic/use-case.php
+++ epic/use-case.php
@@ -10,6 +10,33 @@
 		<p></p>
 
 
+		<h2>Kristi Wilkins - Pet Finder</h2>
+		<h3>Use Case&#58;</h3>
+		<p>Kristi found a dog while on her commute home from work. She is looking for a way to let other people know so that she
+			can return the dog to its rightful owner.</p>
+		<h3>User Story&#58;</h3>
+		<p>Kristi needs a website that allows her to make a post with relevant information on the dog, so she can
+		get in contact with the owner and return the dog.</p>
+		<h3>Frustrations&#58;</h3>
+		<ul>
+			<li>Not being able to post a picture of the dog.</li>
+			<li>Unable to mark the region where the dog was found.</li>
+			<li>Unable to easily correspond with the owner.</li>
+		</ul>
+		<h3>Interaction Flow&#58;</h3>
+		<ul>
+			<li>Kristi arrives at the homepage.</li>
+			<li>The homepage has a button that says "Found A Lost Animal? Click Here!"</li>
+			<li>A small popup appears asking her if she would like to make an account through Facebook, Google, or e-mail.</li>
+			<li>Kristi decides to log in through Facebook for easy communication.</li>
+			<li>Kristi is then brought to a new webpage.</li>
+			<li>The webpage displays a space to upload a picture as well as multiple lines to fill out name, breed, etc.</li>
+			<li>She fills out the information and scrolls down to a map with the ability to pin the area where the dog was found.</li>
+			<li>She finalizes the post and waits to see if the owner gets in contact with her.</li>
+			<li>A few days later Kristi receives a message via Facebook from Walter Lawrence.</li>
+			<li>They schedule a time and place to meet so the dog can be returned to Walter.</li>
+			<li>Walter is reunited with his dog and Kristi changes the status of her post to returned!</li>
+		</ul>
 
 	</body>
 </html>
