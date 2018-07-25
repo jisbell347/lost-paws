9e548c13f770f85967fe54431dd242767ea0cfe8
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-25 15:28:22 -0600

    Added persona name, basic information and attitude

--- epic/persona.php
+++ epic/persona.php
@@ -5,8 +5,25 @@
 		<title>Lost Paws Persona</title>
 	</head>
 	<body>
-		<h4>Bill</h4>
-		<p> Bill lost his cat.</p>
+		<div>
+			<h1>Kristi Wilkins</h1>
+			<img src="./images/kristi-wilkins.jpg" alt="Image of Kristi Wilkins">
+			<div>
+				<h2>Persona</h2>
+					<p><span>Name:</span> Kristi Wilkins</p>
+					<p><span>Gender:</span> Female</p>
+					<p><span>Age:</span> 24</p>
+					<p><span>Marital Status:</span> Single</p>
+					<p><span>Profession:</span> Sous Chef</p>
+			</div>
+			<div>
+				<h2>Attitude</h2>
+				<p>Kristi is a huge animal lover and attends many pet adoption events on weekends. She believes it is her
+					mission to reunite missing pets with their owners. She would like a place where she can post information
+					about the many lost animals she has found and return them home.</p>
+			</div>
+
+
 
 	</body>
 </html>
