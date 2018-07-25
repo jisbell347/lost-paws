a261df30bcbe2c1a3dd463985c3e9b744f6a523e
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-25 15:38:14 -0600

    Added link to stylesheet

--- epic/persona.php
+++ epic/persona.php
@@ -3,11 +3,12 @@
 	<head lang="en">
 		<meta charset="UTF-8">
 		<title>Lost Paws Persona</title>
+		<link rel="stylesheet" href="./css/styles.css" type="text/css">
 	</head>
 	<body>
 		<div>
 			<h1>Kristi Wilkins</h1>
-			<img src="./images/kristi-wilkins.jpg" alt="Image of Kristi Wilkins">
+			<img src="./images/kristi-wilkins.jpg" alt="Image of Kristi Wilkins" class="finder-persona-image">
 			<div>
 				<h2>Persona</h2>
 					<p><span>Name:</span> Kristi Wilkins</p>
@@ -21,6 +22,14 @@
 				<p>Kristi is a huge animal lover and attends many pet adoption events on weekends. She believes it is her
 					mission to reunite missing pets with their owners. She would like a place where she can post information
 					about the many lost animals she has found and return them home.</p>
+			</div>
+			<div>
+				<h2>Needs</h2>
+				<ul>
+					<li>Wants to be able to post information about found pets.</li>
+					<li>Wants to be able to contact owners about pets that she found.</li>
+					<li>Wants to be able to search through missing pets to see if they match any she has found.</li>
+				</ul>
 			</div>
 
 
