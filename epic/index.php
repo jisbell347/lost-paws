4e3124087840be320c80a23c91754818f3336d64
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-25 14:48:42 -0600

    Added initial html and links to other files

---
+++ epic/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Lost Paws Data Design</title>
+		<meta charset="UTF-8">
+	</head>
+	<body>
+		<nav>
+			<ul>
+				<li><a href="persona.php">Persona</a></li>
+				<li><a href="conceptual-model.php">Conceptual Model</a></li>
+				<li><a href="use-case.php">Use Case</a></li>
+			</ul>
+		</nav>
+	</body>
+</html>
