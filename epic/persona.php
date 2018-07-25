a8a1a8cc5bab9d47a19f77a06c0514f775ccda51
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-25 16:02:33 -0600

    Corrected a typo in persona.php

--- epic/persona.php
+++ epic/persona.php
@@ -48,7 +48,7 @@
 					<p><span>Technology:</span>Walter uses a Windows Surface Pro (Intel Core i7; 16GB RAM; 512GB SSD; 12.3" Touchscreen 2736x1824px Display; Windows 10 Pro) at work and a Dell Inspiron 17 5000 Series laptop (Intel Core i7; 16GB RAM; 17.3" 1920x1080px Display; Windows 10 Home) at home. He also owns an Apple iPhone 5S (iOS 7; 16GB Internal Storage; 1136x640px resolution) with an unlimited 4G data connection.</p>
 					<p><span>Attitudes and Behaviors:</span>Yesterday Walter lost his dog, Chico, a three-year old heeler mix. Walter is extremely upset. He will do anything to find his lost friend. At the same time, Walter has little free time on his hands, so he needs a quick and effective way to find Chico. That is why he has turned his attention to <strong>lostpaws.com</strong>.</p>
 					<p><span>Frustrations and Needs:</span>Walter doesn’t like when a site makes him fill too much unnecessary information into a form. It also upsets him when it takes too long to upload his information and/or to get search results. Since Walter is in the middle of a very demanding and time consuming project, he values the simplicity and efficiency of the web site the most.</p>
-					p><span>Goals:</span>At the moment, Walter needs to fill in a form with the information about Chico quickly and upload the picture of him without any issues. He hopes that with the help of lostpaws.com he will find Chico very soon.</p>
+					<p><span>Goals:</span>At the moment, Walter needs to fill in a form with the information about Chico quickly and upload the picture of him without any issues. He hopes that with the help of lostpaws.com he will find Chico very soon.</p>
 				</div>
 			</div>
 
