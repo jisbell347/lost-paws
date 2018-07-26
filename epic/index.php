c614249cbe85203090dfa06192b28c77b4c065ee
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-26 15:01:23 -0600

    Fixed styles for nav element

--- epic/index.php
+++ epic/index.php
@@ -4,15 +4,17 @@
 		<title>Lost Paws Data Design</title>
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="./css/styles.css">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 	</head>
 	<body class="bckground">
 		<header>
 			<nav class="nav">
-				<ul>
+				<ul class="nav-list">
 					<li class="nav-element"><a href="persona.php">Persona</a></li>
 					<li class="nav-element"><a href="conceptual-model.php">Conceptual Model</a></li>
 					<li class="nav-element"><a href="use-case.php">Use Case</a></li>
 				</ul>
+					<h2 class="nav-logo">Lost Paws <i class="fas fa-paw"></i></h2>
 			</nav>
 		</header>
 	</body>
