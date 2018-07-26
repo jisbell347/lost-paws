d4d853dd5f20e14fd22c8c3b8103cea2ab7f6cbc
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-26 14:37:32 -0600

    Added background properties

--- epic/index.php
+++ epic/index.php
@@ -5,13 +5,15 @@
 		<meta charset="UTF-8">
 		<link rel="stylesheet" href="./css/styles.css">
 	</head>
-	<body>
-		<nav class="nav">
-			<ul>
-				<li class="nav-element"><a href="persona.php">Persona</a></li>
-				<li class="nav-element"><a href="conceptual-model.php">Conceptual Model</a></li>
-				<li class="nav-element"><a href="use-case.php">Use Case</a></li>
-			</ul>
-		</nav>
+	<body class="bckground">
+		<header>
+			<nav class="nav">
+				<ul>
+					<li class="nav-element"><a href="persona.php">Persona</a></li>
+					<li class="nav-element"><a href="conceptual-model.php">Conceptual Model</a></li>
+					<li class="nav-element"><a href="use-case.php">Use Case</a></li>
+				</ul>
+			</nav>
+		</header>
 	</body>
 </html>
