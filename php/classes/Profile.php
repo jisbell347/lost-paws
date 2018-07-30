b207de4a74980b0a0c7058bf938e83d10daa4f77
Asya Nikitina <asya_nikitina@hotmail.com>
2018-07-30 16:55:55 -0600

    Started writing Profile class (Profile.php)

--- php/classes/Profile.php
+++ php/classes/Profile.php
@@ -5,6 +5,51 @@ namespace \namespace\here ;
  * Profile section of the lostpaws.com site. After logging in with oAuth, a user profile is created which displays name and contact info.
  */
 
+
+/*
+ *
+ *
+    public function __construct($newUserId, string $newUserEmail, string $newUserHash, string $newUserName) {
+		try {
+			$this->setUserId($newUserId);
+			$this->setUserEmail($newUserEmail);
+			$this->setUserHash($$newUserHash);
+			$this->setUserName($newUserName);
+		} catch (\InvalidArgumentException | \RangeException | \TypeError | \Exception $e) {
+			// rethrow uncought by the mutators exceptions
+			throw(new $exceptionType($e->getMessage(), 0, $e));
+		}
+	}
+
+ */
+
 class Profile {
+	private $profileId;
+	private $profileOAuthId;
+	private $profileAccessToken;
+	private $profileEmail;
+	private $profileName;
+	private $profilePhone;
+
+	/*
+	 * TODO: find out about access token
+	 */
+	public function __construct($newProfileId, int $newProfileOAuthId, ?string $newProfileAccessToken,
+										 string $newProfileEmail, string $newProfileName, ?string $newProfilePhone) {
+		try {
+			$this->setProfileId($newProfileId);
+			$this->setProfileOAuthId($newProfileOAuthId);
+			$this->setProfileAccessToken($newProfileAccessToken);
+			$this->setProfileEmail($newProfileEmail);
+			$this->setProfileName($newProfileName);
+			$this->setProfilePhone($newProfilePhone);
+		} cath () {
+
+		}
+	}
+
+
+
+
 
 }
