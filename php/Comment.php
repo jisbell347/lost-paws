6caac47b48b35d1b2474df6c3fbf7d33ab3065e3
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-30 14:58:04 -0600

    Created Comment class file and added initial doc block, namespace and class name

---
+++ php/Comment.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace \namespace\here;
+
+/*
+ * Comment section of the lostpaws.com site. A user will be able to post comments about the animal to contact other people in regards to the status of the animal.
+ */
+
+class Comment {
+
+}
