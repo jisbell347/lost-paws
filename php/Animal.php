d508394a1273a0718cd6d232ac8b0dec0c0560c7
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-30 14:57:48 -0600

    Created Animal class file and added initial doc block, namespace and class name

---
+++ php/Animal.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace \namespace\here;
+
+/*
+ * Animal section of the lostpaws.com site. After logging in, a user can post information about the animal including: status, color, name, species, gender and location as well as upload an image of the animal.
+ */
+
+class Animal {
+
+}
