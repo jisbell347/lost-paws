8d76a67b4b2ab3c624fcf48d4fda95035ca3ac51
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-30 16:51:27 -0600

    set animal attributes

--- php/classes/Animal.php
+++ php/classes/Animal.php
@@ -7,5 +7,28 @@ namespace \namespace\here;
  */
 
 class Animal {
+	useValidateUuid;
+	useValidateDate;
+
+	/**
+	 * id for animal: this is the primary key
+	 * @var Uuid $animalId
+	 */
+	private $animalId;
+	/**
+	 * profileId for this animal; this is the foreign key
+	 * @var Uuid $animalProfileId
+	 */
+	private $animalProfileId;
+
+	private $animalColor;
+	private $animalDate;
+	private $animalDescription;
+	private $animalGender;
+	private $animalImageUrl;
+	private $animalLocation;
+	private $animalName;
+	private $animalSpecies;
+	private $animalStatus;
 
 }
