43c2999902f39a41735765f40ccd44158f096c3c
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-30 16:41:20 -0600

    Added use Ramsey and autoloaders

--- php/OAuth.php
+++ php/OAuth.php
@@ -1,12 +1,17 @@
 <?php
 
 namespace \namespace\here;
+require_once("autoload.php");
+require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
+use Ramsey\Uuid\Uuid;
 /*
  *oAuth section of lostpaws.com. The user clicks on an authentication service (Google, Facebook, Github) which then
  * creates a profile on the lostpaws site based on the information gathered from the authentication site of their choice.
  */
 
 class OAuth {
+
+
 
 }
