fcab6df27fbfd1a53d1b43315a19fc00dcc35e9a
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-30 14:59:40 -0600

    Created Profile class file and added initial doc block, namespace and class name

---
+++ php/Profile.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace \namespace\here ;
+/*
+ * Profile section of the lostpaws.com site. After logging in with oAuth, a user profile is created which displays name and contact info.
+ */
+
+class Profile {
+
+}
