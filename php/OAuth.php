6a12adb33305710354e8e79dc7f1e14b525ec42c
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-30 14:59:14 -0600

    Created OAuth class file and added initial doc block, namespace and class name

---
+++ php/OAuth.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace  Namespace/here ;
+
+/*
+ *oAuth section of lostpaws.com. The user clicks on an authentication service (Google, Facebook, Github) which then
+ * creates a profile on the lostpaws site based on the information gathered from the authentication site of their choice.
+ */
+
+class OAuth {
+
+}
