30d81ad7094aab9f37d46f5bc0f157a1a1596077
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-30 17:05:03 -0600

    Added state variables for oAuth class

--- php/classes/OAuth.php
+++ php/classes/OAuth.php
@@ -5,13 +5,34 @@ require_once("autoload.php");
 require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
 
 use Ramsey\Uuid\Uuid;
-/*
- *oAuth section of lostpaws.com. The user clicks on an authentication service (Google, Facebook, Github) which then
+/**
+ *oAuth section of lostpaws.com.
+ *
+ * The user clicks on an authentication service (Google, Facebook, Github) which then
  * creates a profile on the lostpaws site based on the information gathered from the authentication site of their choice.
- */
+ * @author Joseph Isbell <jisbell1@cnm.edu>
+ * @version 1.0.0
+ **/
 
 class OAuth {
+	use ValidateDate;
+	use ValidateUuid;
 
+	/**
+	 * id for this OAuth; This is the primary key
+	 * @var int $oAuthId
+	 **/
+	private $oAuthId;
 
+	/**
+	 * Source where the data from the OAuth is being accessed from
+	 * @var string $oAuthSource
+	 **/
+	private $oAuthSource;
 
+	/**
+	 * Constructor for the oAuth
+	 *
+	 * @param string|Uuid $newOAuthId id of the oAuth or null if a new oAuth
+	 */
 }
