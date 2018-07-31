7d8b1f0b107824f61df8b9e7cf710314a9f82bdf
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-30 19:20:34 -0600

    Added constructor and accessor/mutator methods for state variables

--- php/classes/OAuth.php
+++ php/classes/OAuth.php
@@ -20,7 +20,7 @@ class OAuth {
 
 	/**
 	 * id for this OAuth; This is the primary key
-	 * @var int $oAuthId
+	 * @var int unsigned $oAuthId
 	 **/
 	private $oAuthId;
 
@@ -33,6 +33,69 @@ class OAuth {
 	/**
 	 * Constructor for the oAuth
 	 *
-	 * @param string|Uuid $newOAuthId id of the oAuth or null if a new oAuth
+	 * @param int $newOAuthId id of the oAuth or null if a new oAuth
+	 * @param string $newOAuthSource the source the oAuth is getting the data from.
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if values exceed the preset bounds (e.g.; the strings have too many characters)
+	 * @throws \Exception if some other exception occurs
+	 * @throws \TypeError if data violates the type hints
+	 * @Documentation https:/php.net/manuel/en/languqge.oop5.decon.php
 	 */
+
+	public function __construct(int $newOAuthId, string $newOAuthSource) {
+		try {
+			$this->setOAuthId($newOAuthId);
+			$this->setOAuthSource($newOAuthSource);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw (new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+	}
+
+	/**
+	 * The accessor method for the oAuthId
+	 *
+	 * @return int value of oAuth id
+	 **/
+	public function getOAuthId() : int {
+		return($this->oAuthId);
+	}
+
+	/**
+	 * The mutator method for the oAuth id
+	 *
+	 * @param int unsigned $newOAuthId new value of oAuth id
+	 * @throws \RangeException if $newOAuthId is not positive
+	 * @throws \TypeError if $newOAuthId is not an integer
+	 **/
+	public function setOAuthId(int $newOAuthId) : void {
+		//Verifies that the oAuth Id is a positive number
+		if($newOAuthId <= 0) {
+			throw (new \InvalidArgumentException("Id cannot be less than or equal to zero");)
+		}
+		//Stores the value if passes validation
+		$this->oAuthId = $newOAuthId;
+	}
+
+	/**
+	 *The accessor method for the oAuth source
+	 *
+	 *@returns string value of oAuth source
+	 **/
+	public function getOAuthSource() : string {
+		return($this->oAuthSource);
+	}
+
+	/**
+	 * The mutator method for the oAuth source
+	 *
+	 * @param string $newOAuthSource new value of oAuth source
+	 * @throws \RangeException if $newOAuthSource is > 16 characters
+	 * @throws \TypeError if $newOAuthSource is not a string
+	 **/
+
+	public function setOAuthSource(string $newOAuthSource) : void {
+
+	}
+
 }
