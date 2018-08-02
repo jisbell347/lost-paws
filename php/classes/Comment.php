e529d85c5a1533132e4eeff14f228adf6cccfcfd
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-01 20:40:34 -0600

    Added \Exception to docbloc in setCommentDate https://app.asana.com/0/762135285084990/762313207814138

--- php/classes/Comment.php
+++ php/classes/Comment.php
@@ -153,6 +153,7 @@ class Comment {
 	 * @param \DateTime|string|null $newCommentDate comment date as a DateTime object or string (or null to load the current time)
 	 * @throws \InvalidArgumentException if $newCommentDate is not a valid object or string
 	 * @throws \RangeException if $newCommentDate is a date that does not exist
+	 * @throws \Exception if some other error occurs
 	 **/
 	public function setCommentDate($newCommentDate = null): void {
 		//base case: if the date is null, use the current date and time
