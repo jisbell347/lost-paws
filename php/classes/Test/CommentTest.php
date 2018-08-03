10024d88f72b991708d1f33475318f901324d084
Adel Moreno <adel.argenix@gmail.com>
2018-08-03 11:51:27 -0600

    fixed set up statement and state variables fixed order of tests
    https://app.asana.com/0/762135285084990/762313207814138

--- php/classes/Test/CommentTest.php
+++ php/classes/Test/CommentTest.php
@@ -1,7 +1,14 @@
 <?php
 namespace Jisbell347\LostPaws\Test;
 
-use Jisbell347\LostPaws\Comment;
+use Jisbell347\LostPaws\{
+	Animal,
+	OAuth,
+	Profile,
+	Comment
+};
+
+
 //grab the class under scrutiny
 require_once(dirname(__DIR__) . "/autoload.php");
 
@@ -19,16 +26,23 @@ require_once(dirname(__DIR__, 2) . "/uuid.php");
  **/
 class CommentTest extends LostPawsTest {
 	/**
-	 * Animal that the Comment was made on, this is for foreign key relations
-	 * @var Animal $animal
-	 **/
-	protected $animal;
+	 * OAuth for profile
+	 * @var OAuth $OAuth
+	 */
+
+	protected $oAuth = null;
 
 	/**
 	 * Profile that created the Comment; this is for foreign key relations
 	 * @var Profile $profile
 	 **/
-	protected $profile;
+	protected $profile = null;
+
+	/**
+	 * Animal that the Comment was made on, this is for foreign key relations
+	 * @var Animal $animal
+	 **/
+	protected $animal = null;
 
 	/**
 	 * text of the Comment
@@ -53,13 +67,19 @@ class CommentTest extends LostPawsTest {
 	 **/
 
 	public final function setUp(): void {
+		parent::setUp();
 		//create and insert an Animal to receive the the test Comment
 
 		// create and insert a Profile to own the test Comment
-		/**
-		 * NEED TO ASK ABOUT THIS LINE, NOT SURE HOW TO STRUCTURE AND CAUSING ERRORS :(
-		 **/
-		$this->profile = new Profile(generateUuidV4(), null, "username", );
+
+		$this->oAuth = new OAuth($this->oAuth->getOAuthId(), "facebook");
+		$this->oAuth->insert($this->getPDO());
+
+		$this->profile = new Profile($this->profile->getProfileId(), $this->profile->getProfileOAuthId(), "woof", "catsanddogs@found.com", "TheGoodestBoy", "1234567890");
+		$this->profile->insert($this->getPDO());
+
+		$this->animal = new Animal($this->animal->getAnimalId(), $this->animal->getAnimalProfileId(), "white", $this->animal->getAnimalDate(), "Cocker Spaniel Dachsund Mix", "Female", "https://placedog.net/520", "Downtown Albuquerque", "Lady", "dog", "lost");
+		$this->animal->insert($this->getPDO());
 
 		// calculate the date (just use the time the unit test was setup)
 		$this->VALID_COMMENTDATE = new \DateTime();
@@ -149,10 +169,10 @@ class CommentTest extends LostPawsTest {
 		$this->assertNull($comment);
 	}
 
+
 	/**
 	 * test inserting a Comment and regrabbing it from mySQL
 	 **/
-
 	public function testGetValidCommentByCommentProfileId() {
 		// count the number of rows and save it for later
 		$numRows = $this->getConnection()->getRowCount("comment");
@@ -187,4 +207,42 @@ class CommentTest extends LostPawsTest {
 		$comment = Comment::getCommentByCommentProfileId($this->getPDO(), generateUuidV4());
 		$this->assertCount(0, $comment);
 	}
+	/**
+	 * test grabbing a comment by animal id
+	 **/
+
+	public function testGetValidCommentByCommentAnimalId() {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("comment");
+
+		// create a new Comment and insert it into mySQL
+		$commentId = generateUuidV4();
+		$comment = new Comment($commentId, $this->animal->getAnimalId(), $this->profile->getProfileId(), $this->VALID_COMMENTTEXT, $this->VALID_COMMENTDATE);
+		$comment->insert($this->getPDO());
+
+		// grab the data from mySQL and ensure the fields match our expectations
+		$results = Comment::getCommentByCommentAnimalId($this->getPDO(), $comment->getCommentAnimalId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("comment"));
+		$this->assertCount(1, $results);
+		$this->assertContainsOnlyInstancesOf("Jisbell347\LostPaws\Comment", $results);
+
+		//grab the result from the array and validate it
+		$pdoComment = $results[0];
+
+		$this->assertEquals($pdoComment->getCommentId(), $commentId);
+		$this->assertEquals($pdoComment->getCommentAnimalId(), $this->animal->getAnimalId());
+		$this->assertEquals($pdoComment->getCommentProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoComment->getCommentText(), $this->VALID_COMMENTTEXT);
+		// format the date to seconds since the beginning of time to avoid round off error
+		$this->assertEquals($pdoComment->getCommentDate()->getTimestamp(), $this->VALID_COMMENTDATE->getTimestamp());
+
+	}
+	/**
+	 * test grabbing a comment that does not exist
+	 **/
+	public function testGetInvalidCommentByCommentAnimalId() : void {
+		// grab a profile id that exceeds the maximum allowable comment animal id
+		$comment = Comment::getCommentByCommentAnimalId($this->getPDO(), generateUuidV4());
+		$this->assertCount(0, $comment);
+
 }
