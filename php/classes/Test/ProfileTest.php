c199b6d6e0ef9643670c774f221a7c79c0302c1e
Asya Nikitina <anikitina@cnm.edu>
2018-08-05 10:46:31 -0600

    Continue working on test functions in ProfileTest class https://app.asana.com/0/762135285084990/762313207814136

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -3,8 +3,7 @@ namespace Jisbell347\LostPaws\Test;
 // grab the class under scrutiny
 require_once (dirname(__DIR__) . "/autoload.php");
 //require_once(dirname(__DIR__, 2) . "/vendor/autoload.php");
-use Jisbell347\LostPaws\OAuth;
-use Jisbell347\LostPaws\Profile;
+use Jisbell347\LostPaws\{OAuth, Profile};
 use Ramsey\Uuid;
 /**
  * Full PHPUnit test for the Profile class
@@ -41,12 +40,22 @@ class ProfileTest extends LostPawsTest {
 	 **/
 	public function createProfile() : Profile {
 		$this->VALID_ACCESS_TOKEN = bin2hex(random_bytes(16));
-		return(new Profile(generateUuidV4(), $this->oAuth, $this->VALID_ACCESS_TOKEN, $this->VALID_EMAIL1, $this->VALID_NAME1, $this->VALID_PHONE1));
+		$this->$VALID_ACCESS_TOKEN = generateUuidV4();
+		$profile = (new Profile($this->$VALID_ACCESS_TOKEN, $this->oAuth, $this->VALID_ACCESS_TOKEN, $this->VALID_EMAIL1, $this->VALID_NAME1, $this->VALID_PHONE1);
+		// make sure that accessors return the expected properties
+		$this->assertEquals($profile->getProfileId(), $this->$VALID_ACCESS_TOKEN);
+		$this->assertEquals($profile->getProfileOAuthId(), $this->oAuth);
+		$this->assertEquals($profile->getProfileAccessToken(), $this->VALID_ACCESS_TOKEN);
+		$this->assertEquals($profile->getProfileEmail(), $this->VALID_EMAIL1);
+		$this->assertEquals($profile->getProfileName(), $this->VALID_NAME1);
+		$this->assertEquals($profile->getProfilePhone(), $this->VALID_PHONE1);
+
+		return($profile);
 	}
 
-
 	/**
-	 * insert a valid Profile and verify that the actual mySQL data matches
+	 * insert a valid Profile and verify a new record appeared in the database
+	 * @depends createProfile
 	 **/
 	public function testInsertValidProfile() : void {
 		// count the number of rows and save it for later
@@ -55,19 +64,41 @@ class ProfileTest extends LostPawsTest {
 		$profile = $this->createProfile();
 		$profile->insert($this->getPDO());
 
+		// make sure that the record is inserted
+		$this->assertEquals($numRows+1, $this->getConnection()->getRowCount("profile"));
+	}
+
+	/**
+	 * test grabing a Profile from the database using a valid Profile ID
+	 * @depends testInsertValidProfile
+	 **/
+	public function testGetProfileByValidId() : void {
+		// create an instance of the Profile class and insert it into the database
+		$profile = $this->createProfile();
+		$profile->insert($this->getPDO());
+
 		// grab the data from mySQL and enforce the fields match our expectations
 		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
+		$this->assertEquals($pdoProfile->getProfileId(), $profile->getProfileId());
+		$this->assertEquals($pdoProfile->getProfileAccessToken(), $profile->getProfileAccessToken());
+		$this->assertEquals($pdoProfile->getProfileEmail(), $profile->getProfileEmail());
+		$this->assertEquals($pdoProfile->getProfileName(), $profile->getProfileName());
+		$this->assertEquals($pdoProfile->getProfilePhone(), $profile->getProfilePhone());
+	}
 
-		$this->assertEquals($numRows+1, $this->getConnection()->getRowCount("profile"));
-		$this->assertEquals($pdoProfile->getProfileId(), $profileId);
-		$this->assertEquals($pdoProfile->getProfileAccessToken(), $this->VALID_ACCESS_TOKEN);
-		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_EMAIL1);
-		$this->assertEquals($pdoProfile->getProfileName(), $this->VALID_NAME1);
-		$this->assertEquals($pdoProfile->getProfilePhone(), $this->VALID_PHONE1);
+	/**
+	 * test grabing a Profile that doesn't exist from the database using a valid Profile ID
+	 **/
+	public function testGetProfileByProfileId() : void {
+		$wrongProfileId = generateUuidV4();
+		// try to grab a record with wrong profile ID
+		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
+		$this->assertNull($pdoProfile);
 	}
 
 	/**
 	 * insert a valid Profile, edit it, and update it and then verify that the actual mySQL data matches
+	 * @depends createProfile
 	 **/
 	public function testUpdateValidProfile() :void {
 		// count the number of rows and save it for later
@@ -85,8 +116,8 @@ class ProfileTest extends LostPawsTest {
 		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
 
 		$this->assertEquals($numRows+1, $this->getConnection()->getRowCount("profile"));
-		$this->assertEquals($pdoProfile->getProfileId(), $profileId);
-		$this->assertEquals($pdoProfile->getProfileAccessToken(), $this->VALID_ACCESS_TOKEN);
+		$this->assertEquals($pdoProfile->getProfileId(),  $profile->getProfileId());
+		$this->assertEquals($pdoProfile->getProfileAccessToken(),  $profile->getProfileAccessToken());
 		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_EMAIL2);
 		$this->assertEquals($pdoProfile->getProfileName(), $this->VALID_NAME2);
 		$this->assertEquals($pdoProfile->getProfilePhone(), $this->VALID_PHONE2);
@@ -94,28 +125,29 @@ class ProfileTest extends LostPawsTest {
 
 	/**
 	 * insert a valid Profile into the database and then delete it
+	 * @depends testInsertValidProfile
 	 **/
 	public function testDeleteValidProfile() : void {
-		// count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("profile");
 		// create an instance of the Profile class and insert it into the database
 		$profile = $this->createProfile();
 		$profile->insert($this->getPDO());
-		// check if it was inserted successfully
-		$this->assertEquals($numRows+1, $this->getConnection()->getRowCount("profile"));
+		// count the number of rows after the profile was inserted
+		$numRows = $this->getConnection()->getRowCount("profile");
+		// save profile ID that is going to be deleted
 		$currProfileId = $profile->getProfileId();
-		// delete it
+		// delete this profile
 		$profile->delete($this->getPDO());
-		// try to find it in the database
-		$pdoProfile Profile::getProfileByProfileId($this->getPDO(), $currProfileId);
+		// check if this profile is still in the database
+		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $currProfileId);
 		$this->assertNull($pdoProfile);
-		$this->assertEquals($numRows, $this->getConnection()->getRowCount("profile"));
+		$this->assertEquals($numRows-1, $this->getConnection()->getRowCount("profile"));
 	}
 
 	/**
 	 * grab a Profile from the database using a valid Profile ID
 	 **/
 	public function testGetProfileByValidId() : void {
+
 
 	}
 	/**
@@ -159,6 +191,8 @@ class ProfileTest extends LostPawsTest {
 		getProfileByProfileId
 		getProfileByProfileOAuthId
 		getProfileByProfileEmail
+
+	fake email: yesyes@nonono.com
 		*/
 
 	/*
