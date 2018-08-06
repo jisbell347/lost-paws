66b8f8c016643a17125b454fcc95cce1bf2f7c07
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-05 20:05:46 -0600

    Added todos and comments https://app.asana.com/0/762135285084990/762313207814138

--- php/classes/Test/CommentTest.php
+++ php/classes/Test/CommentTest.php
@@ -13,7 +13,7 @@ use Jisbell347\LostPaws\{
 require_once(dirname(__DIR__) . "/autoload.php");
 
 // grab the uuid generator
-require_once(dirname(__DIR__, 2) . "/uuid.php");
+require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
 
 /**
  * Full PHPUnit test for the Comment class
