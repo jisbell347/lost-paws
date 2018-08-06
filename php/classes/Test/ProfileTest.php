ae3b924ba887abb473aec4bca606fd99afb563c9
Asya Nikitina <anikitina@cnm.edu>
2018-08-06 16:10:20 -0600

    Started writing the Test file from scratch https://app.asana.com/0/762135285084990/762313207814136

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -22,8 +22,9 @@ class ProfileTest extends LostPawsTest {
 	/**
 	 * declare state variables
 	 */
+
 	protected $VALID_PROFILE_ID;
-	protected $oAuth = 100;
+	protected $oAuth = null;
 	protected $VALID_ACCESS_TOKEN;
 	protected $VALID_EMAIL1 = "b.smith@gmail.com";
 	protected $VALID_EMAIL2 = "j.martin@yahoo.com";
@@ -32,153 +33,38 @@ class ProfileTest extends LostPawsTest {
 	protected $VALID_PHONE1 = "+1 (505) 555-5555";
 	protected $VALID_PHONE2 = "+1-222-333-4455";
 
-	/*
+
 	public final function setUp() {
 		parent::setUp();
-	}
-*/
-	/**
-	 * instantiate a valid Profile object and return it
-	 **/
+
+		$oAuth = new OAuth(null, "google");
+		$oAuth->insert($this->getPDO());
+		}
+
 	public function createProfile() : Profile {
-		// set up ID fields for primary and foreign keys
-		$this->VALID_PROFILE_ID = bin2hex(random_bytes(16));
-		$this->VALID_ACCESS_TOKEN = generateUuidV4();
-		$profile = new Profile($this->VALID_PROFILE_ID, $this->oAuth, $this->VALID_ACCESS_TOKEN, $this->VALID_EMAIL1, $this->VALID_NAME1, $this->VALID_PHONE1);
-		// make sure that accessors return the expected properties
-		//$this->assertEquals($profile->getProfileId(), $this->VALID_PROFILE_ID);
+		$VALID_PROFILE_ID = bin2hex(random_bytes(16));
+		$VALID_ACCESS_TOKEN = generateUuidV4();
+		$profile = new Profile($this->VALID_PROFILE_ID, $this->$oAuth, $this->VALID_ACCESS_TOKEN, $this->VALID_EMAIL1, $this->VALID_NAME1, $this->VALID_PHONE1);
+		$this->assertNotNull($profile);
+		return($profile);
+	}
+
+	public function testValidProfileCreated() : void {
+		$profile = $this->createProfile();
+
+		$this->assertEquals($profile->getProfileId(), $this->VALID_PROFILE_ID);
 		$this->assertEquals($profile->getProfileOAuthId(), $this->oAuth);
 		$this->assertEquals($profile->getProfileAccessToken(), $this->VALID_ACCESS_TOKEN);
 		$this->assertEquals($profile->getProfileEmail(), $this->VALID_EMAIL1);
 		$this->assertEquals($profile->getProfileName(), $this->VALID_NAME1);
 		$this->assertEquals($profile->getProfilePhone(), $this->VALID_PHONE1);
-
-		return($profile);
 	}
 
-	/**
-	 * test inserting a valid Profile and verifying a new record appeared in the database
-	 * @depends createProfile
-	 **/
-	public function testInsertValidProfile() : void {
-		// count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("profile");
-		// create an instance of the Profile class and insert it into the database
-		$profile = $this->createProfile();
-		$profile->insert($this->getPDO());
 
-		// make sure that the record is inserted
-		$this->assertEquals($numRows+1, $this->getConnection()->getRowCount("profile"));
-	}
 
-	/**
-	 * test grabing a Profile from the database using a valid Profile ID
-	 **/
-	public function testGetProfileByValidId() : void {
-		// create an instance of the Profile class and insert it into the database
-		$profile = $this->createProfile();
-		$profile->insert($this->getPDO());
 
-		// grab the data from mySQL and enforce the fields match our expectations
-		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
-		$this->assertEquals($pdoProfile->getProfileId(), $profile->getProfileId());
-		$this->assertEquals($pdoProfile->getProfileOAuthId(), $profile->getProfileOAuthId());
-		$this->assertEquals($pdoProfile->getProfileAccessToken(), $profile->getProfileAccessToken());
-		$this->assertEquals($pdoProfile->getProfileEmail(), $profile->getProfileEmail());
-		$this->assertEquals($pdoProfile->getProfileName(), $profile->getProfileName());
-		$this->assertEquals($pdoProfile->getProfilePhone(), $profile->getProfilePhone());
-	}
 
-	/**
-	 * test grabing a Profile (that doesn't exist) from the database using a valid but wrong Profile ID
-	 **/
-	public function testGetProfileByWrongProfileId() : void {
-		$wrongProfileId = generateUuidV4();
-		// make sure that our test database has at least one record
-		$profile = $this->createProfile();
-		$profile->insert($this->getPDO());
-		// try to grab a record with wrong profile ID
-		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
-		$this->assertNull($pdoProfile);
-	}
 
-	/**
-	 * test inserting a valid Profile, editing it, updating it and then verifying that the actual mySQL data matches
-	 **/
-	public function testUpdateValidProfile() :void {
-		// count the number of rows and save it for later
-		$numRows = $this->getConnection()->getRowCount("profile");
-		// create an instance of the Profile class and insert it into the database
-		$profile = $this->createProfile();
-		$profile->insert($this->getPDO());
-		// edit three different properties in the current Profile object
-		$profile->setProfileEmail($this->VALID_EMAIL2);
-		$profile->setProfileName($this->VALID_NAME2);
-		$profile->setProfilePhone($this->VALID_PHONE2);
-		// update the current profile record in the database
-		$profile->update($this->getPDO());
-		// grab the record from the database and check if each field matches the correspondent value
-		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $profile->getProfileId());
 
-		$this->assertEquals($numRows+1, $this->getConnection()->getRowCount("profile"));
-		$this->assertEquals($pdoProfile->getProfileId(),  $profile->getProfileId());
-		$this->assertEquals($pdoProfile->getProfileOAuthId(), $profile->getProfileOAuthId());
-		$this->assertEquals($pdoProfile->getProfileAccessToken(),  $profile->getProfileAccessToken());
-		$this->assertEquals($pdoProfile->getProfileEmail(), $this->VALID_EMAIL2);
-		$this->assertEquals($pdoProfile->getProfileName(), $this->VALID_NAME2);
-		$this->assertEquals($pdoProfile->getProfilePhone(), $this->VALID_PHONE2);
-	}
 
-	/**
-	 * teset inserting a valid Profile into the database and then deleting it
-	 **/
-	public function testDeleteValidProfile() : void {
-		// create an instance of the Profile class and insert it into the database
-		$profile = $this->createProfile();
-		$profile->insert($this->getPDO());
-		// count the number of rows after the profile was inserted
-		$numRows = $this->getConnection()->getRowCount("profile");
-		// save profile ID that is going to be deleted
-		$currProfileId = $profile->getProfileId();
-		// delete this profile
-		$profile->delete($this->getPDO());
-		// check if this profile is still in the database
-		$pdoProfile = Profile::getProfileByProfileId($this->getPDO(), $currProfileId);
-		$this->assertNull($pdoProfile);
-		$this->assertEquals($numRows-1, $this->getConnection()->getRowCount("profile"));
-	}
-
-	/**
-	 * test grabing a Profile from the database using a valid email address
-	 **/
-	public function testGetProfileByValidEmail() : void {
-		// create an instance of the Profile class and insert it into the database
-		$profile = $this->createProfile();
-		$profile->insert($this->getPDO());
-
-		// try grabing the data from mySQL and check if the fields match our expectations
-		$pdoProfile = Profile::getProfileByProfileEmail($this->getPDO(), $profile->getProfileEmail());
-		$this->assertEquals($pdoProfile->getProfileId(), $profile->getProfileId());
-		$this->assertEquals($pdoProfile->getProfileOAuthId(), $profile->getProfileOAuthId());
-		$this->assertEquals($pdoProfile->getProfileAccessToken(), $profile->getProfileAccessToken());
-		$this->assertEquals($pdoProfile->getProfileEmail(), $profile->getProfileEmail());
-		$this->assertEquals($pdoProfile->getProfileName(), $profile->getProfileName());
-		$this->assertEquals($pdoProfile->getProfilePhone(), $profile->getProfilePhone());
-	}
-
-	/**
-	 * test grabing a Profile from the database using a wrong email address
-	 **/
-	/*
-	public function testGetProfileByInvalidEmail() : void {
-		// fake invalid email address
-		$invalidEmail = "yesyes@nonono.com";
-		// try to create a profile with an invalid email address and insert it into the database
-		$profile = new Profile(bin2hex(random_bytes(16)), $this->oAuth, generateUuidV4(), $invalidEmail, $this->VALID_NAME1, $this->VALID_PHONE1);
-		$profile->insert($this->getPDO());
-		// try to grab a record with wrong email address (there is should not be such a record)
-		$pdoProfile = Profile::getProfileByProfileEmail($this->getPDO(), $invalidEmail);
-		$this->assertNull($pdoProfile);
-	}
-*/
 }
