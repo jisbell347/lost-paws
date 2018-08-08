1d76b8fd0b981f57995e1ec927fa749a90e8cda7
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-08 08:44:07 -0600

    add test getValidAnimalByProfileId https://app.asana.com/0/762135285084990/762313207814137

--- php/classes/Test/AnimalTest.php
+++ php/classes/Test/AnimalTest.php
@@ -243,6 +243,39 @@ Class AnimalTest extends LostPawsTest{
 		$this->assertNull($animal);
 	}
 	/**
+	 * test grabbing an animal by profileId  and re-grabbing it from mySQL.
+	 **/
+	public function testGetValidAnimalByProfileId() {
+		//count the number of rows
+		$numRows = $this->getConnection()->getRowCount("animal");
+		//create a new Animal and insert into mySQL
+		$animalId = generateUuidV4();
+		$animal = new Animal($animalId, $this->profile->getProfileId(),$this->VALID_ANIMAL_COLOR, $this->VALID_ANIMAL_DATE, $this->VALID_ANIMAL_DESCRIPTION, $this->VALID_ANIMAL_GENDER,$this->VALID_ANIMAL_IMAGE_URL, $this->VALID_ANIMAL_LOCATION, $this->VALID_ANIMAL_NAME, $this->VALID_ANIMAL_SPECIES, $this->VALID_ANIMAL_STATUS);
+		$animal->insert($this->getPDO());
+		//grab the data from mySQL and enforce the fields match expectations.
+		$results= Animal::getAnimalByAnimalProfileId($this->getPDO(), $animal->getAnimalProfileId());
+		$this->assertEquals($numRows + 1, $this->getConnection()->getRowCount("animal"));
+		$this->assertCount(1, $results);
+		//enforce that no other objects are bleeding into the test
+		$this->assertContainsOnlyInstancesOf("Jisbell347\LostPaws\Animal", $results);
+		$pdoAnimal = $results[0];
+		$this->assertEquals($pdoAnimal->getAnimalId(), $animalId);
+		$this->assertEquals($pdoAnimal->getAnimalProfileId(), $this->profile->getProfileId());
+		$this->assertEquals($pdoAnimal->getAnimalColor(), $this->VALID_ANIMAL_COLOR);
+		$this->assertEquals($pdoAnimal->getAnimalDescription(),$this->VALID_ANIMAL_DESCRIPTION);
+		$this->assertEquals($pdoAnimal->getAnimalGender(), $this->VALID_ANIMAL_GENDER);
+		$this->assertEquals($pdoAnimal->getAnimalImageUrl(),$this->VALID_ANIMAL_IMAGE_URL);
+		$this->assertEquals($pdoAnimal->getAnimalLocation(), $this->VALID_ANIMAL_LOCATION);
+		$this->assertEquals($pdoAnimal->getAnimalName(),$this->VALID_ANIMAL_NAME);
+		$this->assertEquals($pdoAnimal->getAnimalSpecies(), $this->VALID_ANIMAL_SPECIES);
+		$this->assertEquals($pdoAnimal->getAnimalStatus(), $this->VALID_ANIMAL_STATUS);
+		//format the date to seconds since the beginning of time to avoid round off error.
+		$this->assertEquals($pdoAnimal->getAnimalDate()->getTimestamp(), $animal->getAnimalDate()->getTimestamp());
+	}
+
+
+
+	/**
 	 * test grabbing an animal by animal color
 	 **/
 	public function testGetAnimalByAnimalColor() : void {
