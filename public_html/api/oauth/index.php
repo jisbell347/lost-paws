121ab5557573efbe6df64d686ea17fc908424a99
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-14 16:56:19 -0600

    Finally got it working. Thanks George! https://app.asana.com/0/762135285084990/777901996972587

---
+++ public_html/api/oauth/index.php
@@ -0,0 +1,65 @@
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use Jisbell347\LostPaws\Profile;
+
+
+
+//Verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+
+/**
+ * Create the client object and set the authorization configuration
+**/
+$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/lostfuzzy.ini");
+$config = readConfig("/etc/apache2/capstone-mysql/lostfuzzy.ini");
+$google = json_decode($config["google"]);
+
+$provider = new League\OAuth2\Client\Provider\Google([
+	"clientId" => $google->clientId,
+	"clientSecret" => $google->secretId,
+	"redirectUri" => "https://bootcamp-coders.cnm.edu/~jisbell1/lost-paws/public_html/api/oauth/"
+]);
+
+
+if(!empty($_GET['error'])) {
+	// Got an error, probably user denied access
+	exit('Got error: ' . htmlspecialchars($_GET['error'], ENT_QUOTES, 'UTF-8'));
+} elseif(empty($_GET['code'])) {
+	// If we don't have an authorization code, then get one
+	$authUrl = $provider->getAuthorizationUrl();
+	$_SESSION['oauth2state'] = $provider->getState();
+	header('Location: ' . $authUrl);
+	exit;
+} elseif(empty($_GET['state']) || ($_GET['state'] !== $_SESSION['oauth2state'])) {
+	//State is invalid, possible CSRF attack in progress
+	unset($_SESSION['oauth2state']);
+	exit('Invalid state');
+} else {
+	//Try to get an access token (using the authorization code grant)
+	$token = $provider->getAccessToken('authorization_code', ['code' => $_GET['code']
+	]);
+	//Optional: Now you have a token you can look up a users profile datat
+	try {
+		//We go an access token, let's now get the owner details
+		$ownerDetails = $provider->getResourceOwner($token);
+	} catch(Exception $e) {
+		//Failed to get the user details
+		exit('Something went wrong: ' . $e->getMessage());
+	}
+	//Use this to interact with an API on the users behalf
+	echo $token->getToken();
+
+	//Use this to get a new access token if the old one expires
+	echo $token->getRefreshToken();
+
+	//Number of seconds until the access token will expire and need refreshing
+	echo $token->getExpires();
+}
+
+
