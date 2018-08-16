a5fae7bc20869bd94b939c00c69ad49af6400ec9
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-16 09:33:28 -0600

    Added code that redirects users to facebook login page. Removed uuid and use Profile class cause unnecessary

--- public_html/api/facebook/index.php
+++ public_html/api/facebook/index.php
@@ -2,10 +2,8 @@
 require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
-require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 
-use Jisbell347\LostPaws\Profile;
 
 /**
  * Facebook login API
@@ -29,108 +27,14 @@ $config = readConfig("/etc/apache2/capstone-mysql/lostfuzzy.ini");
 $facebook = json_decode($config["facebook"]);
 
 $fb = new Facebook\Facebook([
-	'app_id' => $facebook->appId, // Replace {app-id} with your app id
-	'app_secret' => $facebook->secret,
-	'default_graph_version' => 'v3.1',
+	"app_id" => $facebook->clientId,
+	"app_secret" => $facebook->secretId,
+	"default_graph_version" => "v3.1",
 ]);
 
 $helper = $fb->getRedirectLoginHelper();
 
-try {
-	$accessToken = $helper->getAccessToken();
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-	// When Graph returns an error
-	echo 'Graph returned an error: ' . $e->getMessage();
-	exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-	// When validation fails or other local issues
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	exit;
-}
+$permissions = ["email"]; // Optional permissions
+$loginUrl = $helper->getLoginUrl("https://bootcamp-coders.cnm.edu/~jisbell1/lost-paws/public_html/api/facebook/callback.php", $permissions);
 
-if (! isset($accessToken)) {
-	if ($helper->getError()) {
-		header('HTTP/1.0 401 Unauthorized');
-		echo "Error: " . $helper->getError() . "\n";
-		echo "Error Code: " . $helper->getErrorCode() . "\n";
-		echo "Error Reason: " . $helper->getErrorReason() . "\n";
-		echo "Error Description: " . $helper->getErrorDescription() . "\n";
-	} else {
-		header('HTTP/1.0 400 Bad Request');
-		echo 'Bad request';
-	}
-	exit;
-}
-
-// Logged in
-echo '<h3>Access Token</h3>';
-//var_dump($accessToken->getValue());
-
-// The OAuth 2.0 client handler helps us manage access tokens
-$oAuth2Client = $fb->getOAuth2Client();
-
-// Get the access token metadata from /debug_token
-$tokenMetadata = $oAuth2Client->debugToken($accessToken);
-echo '<h3>Metadata</h3>';
-//var_dump($tokenMetadata);
-
-// Validation (these will throw FacebookSDKException's when they fail)
-$tokenMetadata->validateAppId($facebook->appId); // Replace {app-id} with your app id
-// If you know the user ID this access token belongs to, you can validate it here
-//$tokenMetadata->validateUserId('123');
-$tokenMetadata->validateExpiration();
-
-if (! $accessToken->isLongLived()) {
-	// Exchanges a short-lived access token for a long-lived one
-	try {
-		$accessToken = $oAuth2Client->getLongLivedAccessToken($accessToken);
-	} catch (Facebook\Exceptions\FacebookSDKException $e) {
-		echo "<p>Error getting long-lived access token: " . $e->getMessage() . "</p>\n\n";
-		exit;
-	}
-
-	echo '<h3>Long-lived</h3>';
-	//var_dump($accessToken->getValue());
-}
-
-$_SESSION['fb_access_token'] = (string) $accessToken;
-
-try {
-	// Returns a `Facebook\FacebookResponse` object
-	$response = $fb->get('/me?fields=id,name,email,location', $accessToken);
-} catch(Facebook\Exceptions\FacebookResponseException $e) {
-	echo 'Graph returned an error: ' . $e->getMessage();
-	exit;
-} catch(Facebook\Exceptions\FacebookSDKException $e) {
-	echo 'Facebook SDK returned an error: ' . $e->getMessage();
-	exit;
-}
-$user = $response->getGraphUser();
-$userName = $user->getName();
-$userEmail = $user->getEmail();
-
-$_SESSION['fbUserId'] = $user['id'];
-
-
-if(empty($_SESSION["fb_access_token"] || empty($_SESSION['fbUserId']) === true)) {
-	throw(new \InvalidArgumentException("Please return to homepage and sign up or login", 403));
-}
-
-//Check to see if profile already exists in database
-$profile = Profile::getProfileByProfileEmail($pdo, $userEmail);
-
-if(!empty($profile)) {
-	$_SESSION["profile"] = $profile;
-	$profileId = $_SESSION['profile']->getProfileId();
-
-	header('Location: ../..');
-} else {
-	$newProfile = new Profile(generateUuidV4(), 2, null, $userEmail, $userName, "");
-	$newProfile->insert($pdo);
-
-	$profile = Profile::getProfileByProfileEmail($pdo, $userEmail);
-	$_SESSION["profile"] = $newProfile;
-
-	header('Location: ../..');
-}
-
+echo '<a href="' . htmlspecialchars($loginUrl) . '">Log in with Facebook!</a>';
