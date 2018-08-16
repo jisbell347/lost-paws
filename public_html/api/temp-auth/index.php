33d42a560e7d41cef66b81a33fe35a2545f55f2c
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-16 17:07:37 -0600

    Added temp-auth file for testing https://app.asana.com/0/762135285084990/777901996972587

--- public_html/api/temp-auth/index.php
+++ public_html/api/temp-auth/index.php
@@ -3,7 +3,7 @@ require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
-require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+require_once (dirname(__DIR__, 3) . "/php/lib/jwt.php");
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 use Jisbell347\LostPaws\{
 	Profile
@@ -47,6 +47,8 @@ try {
 //	setJwtAndAuthHeader("empty", $object);
 	//echo $profile->getProfileId();
 	//$reply->data = $profile->getProfileId();
+
+	$reply->message = "Sign in was successful.";
 } catch(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
 	$reply->message = $exception->getMessage();
