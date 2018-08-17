6ab63b863a7dd7970dd0e78e3d75c69a9b3469e8
Adel Moreno <adel.argenix@gmail.com>
2018-08-17 12:00:41 -0600

    added code to connect to encrypted mysql connection
    https://app.asana.com/0/762048584353741/764118250103781

--- public_html/api/comment/index.php
+++ public_html/api/comment/index.php
@@ -4,7 +4,6 @@ require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
-//require_once (dirname(__DIR__, 3) . "php/lib/jwt.php");
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once (dirname(__DIR__, 3) . "/php/lib/jwt.php");
 
@@ -32,9 +31,8 @@ $reply->status = 200;
 $reply->data = null;
 
 try {
-	//TODO: WHAT GOES HERE?
 	// grab the mySQL connection
-	//$pdo = connectToEncryptedMySQL("");
+	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/lostfuzzy.ini");
 
 	//determine which HTTP method was used
 	$method = array_key_exists("HTTP_X_HTTP_METHOD", $_SERVER) ? $_SERVER["HTTP_X_HTTP_METHOD"] : $_SERVER["REQUEST_METHOD"];
