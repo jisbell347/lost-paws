0da8feeb2b9fefaf0744321ed68e2d518f7eeec5
Adel Moreno <adel.argenix@gmail.com>
2018-08-18 10:49:42 -0600

    removed get animal id
    https://app.asana.com/0/762048584353741/764118250103781

--- public_html/api/comment/index.php
+++ public_html/api/comment/index.php
@@ -140,7 +140,7 @@ try {
 
 			validateJwtHeader();
 			//create a new comment and insert into the database
-			$comment = new Comment(generateUuidV4(), $requestObject->getAnimalId(), $_SESSION["profile"]->getProfileId()->toString(), null, $requestObject->commentText);
+			$comment = new Comment(generateUuidV4(), $requestObject->commentAnimalId, $_SESSION["profile"]->getProfileId(), null, $requestObject->commentText);
 			$comment->insert($pdo);
 
 			// update comment
