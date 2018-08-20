01c127b69ece535269303e89446bb6822ec53ed5
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-20 16:56:46 -0600

    initial commit of animal interface.

---
+++ src/app/shared/interfaces/animal.ts
@@ -0,0 +1,16 @@
+export class Animal {
+	constructor(
+		public animalId: string,
+		public animalProfileId: string,
+		public animalColor: string,
+		public animalDate: string,
+		public animalDescription: string,
+		public animalGender: string,
+		public animalImageUrl: string,
+		public animalLocation: string,
+		public animalName: string,
+		public animalSpecies: string,
+		public animalStatus: string
+
+	) {}
+}
