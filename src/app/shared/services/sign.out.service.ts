d41a202fd03941979b1f08c5ce114b35243dfd5b
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-22 10:37:11 -0600

    Added service for signing out

---
+++ src/app/shared/services/sign.out.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Status } from "../interfaces/status";
+import { Observable } from "rxjs/internal/Observable";
+
+@Injectable()
+
+export class SignOutService {
+
+	constructor(protected http: HttpClient) {
+
+	}
+
+	private signOutUrl = "api/sign-out";
+
+	signOut() : Observable<Status> {
+		return(this.http.get<Status>(this.signOutUrl));
+	}
+}
