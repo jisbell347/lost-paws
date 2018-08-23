b316a5bc32f226253e5a60a9a110b65c187d65ea
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-23 15:54:06 -0600

    initial commit of static navbar

---
+++ static-ui/navbar.php
@@ -0,0 +1,30 @@
+<?php require_once ("head-utils.php");?>
+
+
+<header>
+	<nav class="navbar navbar-expand-lg navbar-light border-bottom border-secondary shadow-sm bg-light fixed-top" id="navigation-bar" >
+		<a class="navbar-brand text-secondary pl-3" href="#">Lost Paws</a>
+		<button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="icon-bar top-bar"></span>
+			<span class="icon-bar middle-bar"></span>
+			<span class="icon-bar bottom-bar"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNavDropdown">
+			<ul class="navbar-nav ml-auto">
+				<li class="nav-item">
+					<a class="nav-link" href="#link1">link 1<span class="sr-only">(current)</span></a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#link2">link 2</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#link3">link 3</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#link4">link 4</a>
+				</li>
+			</ul>
+		</div>
+		<button class="btn btn-secondary" type="submit">Search</button>
+	</nav>
+</header>
