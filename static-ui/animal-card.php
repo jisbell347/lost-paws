635390c8658c82918db2f0764433442d50901001
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-23 15:35:35 -0600

    Added placeholder html for animal-card

---
+++ static-ui/animal-card.php
@@ -0,0 +1,29 @@
+<?php /*require_once ("head-utils.php");
+		require_once ("navbar.php"); */?>
+
+<main>
+	<div class="container-fluid">
+		<div class="row">
+			<h3 class="text-center">Animal Profile</h3>
+		</div>
+		<div class="card">
+			<img src="http://placekitten.com/300/300" alt="Random image of a cat" class="card-image-top p-1">
+			<h4 class="text-center">About this animal:</h4>
+			<ul class="list-group">
+				<li class="list-group-item">Status: Found</li>
+				<li class="list-group-item">Species: Cat</li>
+				<li class="list-group-item">Name: Fuzzy</li>
+				<li class="list-group-item">Color: White/Brown</li>
+				<li class="list-group-item">Location Lost/Found: 1st and Main</li>
+				<li class="list-group-item">Gender: Male</li>
+			</ul>
+			<p class="p-2">Description: Fuzzy is a three year-old male cat with light brown and white fur. He was last seen near our house on 1st and Main about 10pm on the 18th of August wearing a blue collar.</p>
+			<h4>Contact:</h4>
+			<ul class="list-group">
+				<li class="list-group-item">Name: Walter Smith</li>
+				<li class="list-group-item">Phone: 505-111-5555</li>
+				<li class="list-group-item">Email: Fuzzycatlover@gmail.com</li>
+			</ul>
+		</div>
+	</div>
+</main>
