2262104aa518acfa8f78e7073b7bf61cc8b4e6cf
Asya Nikitina <anikitina@cnm.edu>
2018-08-23 10:24:21 -0600

    Finish writing a profile interface and service (profile.ts and profile.service.ts).

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -12,24 +12,29 @@ export class ProfileService {
 	private profileUrl = "api/profile/";
 
 	//reach out to the profile  API and delete the profile in question
-	seletProfile(profileId: string) : Observable<Statustatus> {
-		return(this.http.delete<Status>(this.profileUrl + profileId));
+	deleteProfile(id: string) : Observable<Status> {
+		return(this.http.delete<Status>(this.profileUrl + id));
 	}
 
 	// call to the Profile API and edit the profile in question
-
-
+	editProfile(profile: Profile) : Observable<Status> {
+		return(this.http.put<Status>(this.profileUrl , profile));
+	}
 
 	// call to the Profile API and get a Profile object by its id
+	getProfile(id: string) : Observable<Profile> {
+		return(this.http.get<Profile>(this.profileUrl + id));
 
+	}
 
 	// call to the Profile API and get a Profile object by its email address
-
+	getProfileByProfileEmail(profileEmail: string) : Observable<Profile> {
+		return(this.http.get<Profile>(this.profileUrl + "?profileEmail=" + profileEmail));
+	}
 
 
 	// call to the Profile API and get a Profile object by its phone number
-
-
-
-
+	getProfileByProfilePhone(profilePhone: string) : Observable<Profile> {
+		return(this.http.get<Profile>(this.profileUrl + "?profilePhone=" + profilePhone));
+	}
 }
