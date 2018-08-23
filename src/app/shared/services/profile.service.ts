01930e2b9eeb11bc97cea8407fc7108fadd7b9e1
Asya Nikitina <anikitina@cnm.edu>
2018-08-23 08:39:48 -0600

    Initial commint of profile.service.ts

---
+++ src/app/shared/services/profile.service.ts
@@ -0,0 +1,37 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs/internal/Observable";
+import {Status} from "../interfaces/status";
+
+import {Animal} from"../interfaces/profile";
+
+
+
+export class ProfileService {
+
+	// define an API endpoint
+	private profileUrl = "api/profile/";
+
+
+
+	//reach out to the profile  API and delete the profile in question
+
+
+
+	// call to the Profile API and edit the profile in question
+
+
+
+	// call to the Profile API and get a Profile object by its id
+
+
+	// call to the Profile API and get a Profile object by its email address
+
+
+
+	// call to the Profile API and get a Profile object by its phone number
+
+
+
+
+}
