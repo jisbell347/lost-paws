7f75a8be8bdf647667359f4e71dd9b507db6fcb4
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-23 15:46:59 -0600

    Removed border and centered text on headers

--- static-ui/animal-card.php
+++ static-ui/animal-card.php
@@ -1,13 +1,14 @@
-<?php require_once ("head-utils.php");
-		require_once ("navbar.php"); ?>
+<?php require_once ("head-utils.php");?>
+
+<?php require_once ("navbar.php"); ?>
 
 <main>
-	<div class="container-fluid">
+	<div class="container">
 		<div class="row">
 			<h3 class="text-center">Animal Profile</h3>
 		</div>
-		<div class="card">
-			<img src="http://placekitten.com/300/300" alt="Random image of a cat" class="card-image-top p-1">
+		<div class="card border border-0">
+			<img src="http://placekitten.com/500/500" alt="Random image of a cat" class="card-image-top p-5 img-fluid mx-auto">
 			<h4 class="text-center">About this animal:</h4>
 			<ul class="list-group">
 				<li class="list-group-item">Status: Found</li>
@@ -16,9 +17,9 @@
 				<li class="list-group-item">Color: White/Brown</li>
 				<li class="list-group-item">Location Lost/Found: 1st and Main</li>
 				<li class="list-group-item">Gender: Male</li>
+				<li class="list-group-item">Description: Fuzzy is a three year-old male cat with light brown and white fur. He was last seen near our house on 1st and Main about 10pm on the 18th of August wearing a blue collar.</li>
 			</ul>
-			<p class="p-2">Description: Fuzzy is a three year-old male cat with light brown and white fur. He was last seen near our house on 1st and Main about 10pm on the 18th of August wearing a blue collar.</p>
-			<h4>Contact:</h4>
+			<h4 class="p-3 text-center">Contact:</h4>
 			<ul class="list-group">
 				<li class="list-group-item">Name: Walter Smith</li>
 				<li class="list-group-item">Phone: 505-111-5555</li>
