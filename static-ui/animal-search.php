1ce3952cd89e427ba77ea9ec2a21c2c0dd3e37a8
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-24 08:58:09 -0600

    Changed active class to first pagintation item

--- static-ui/animal-search.php
+++ static-ui/animal-search.php
@@ -39,16 +39,18 @@
 			</div>
 		</div>
 	</div>
-	<div class="container justify-content-center">
-		<div class="row">
+	<div class="container pt-5">
+		<div class="row justify-content-center">
 			<nav aria-label="...">
 				<ul class="pagination">
 					<li class="page-item disabled">
 						<a class="page-link" href="#" tabindex="-1">Previous</a>
 					</li>
-					<li class="page-item"><a class="page-link" href="#">1</a></li>
 					<li class="page-item active">
-						<a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+						<a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
+					</li>
+					<li class="page-item">
+						<a class="page-link" href="#">2</a>
 					</li>
 					<li class="page-item"><a class="page-link" href="#">3</a></li>
 					<li class="page-item">
