2cf0920d54c5b8c543f4785e12dd3531240283e7
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-24 09:35:29 -0600

    center modal on page. add recaptcha and template text to be shown once recaptcha is verified.

--- static-ui/contact-modal.php
+++ static-ui/contact-modal.php
@@ -6,7 +6,7 @@
 
 	<!-- Modal -->
 	<div id="contact" class="modal" tabindex="-1" role="dialog">
-		<div class="modal-dialog" role="document">
+		<div class="modal-dialog modal-dialog-centered" role="document">
 			<div class="modal-content">
 				<div class="modal-header">
 					<h5 class="modal-title">Contact Information</h5>
@@ -14,8 +14,17 @@
 						<span aria-hidden="true">&times;</span>
 					</button>
 				</div>
-				<div class="modal-body">
-					<p>Modal body text goes here.</p>
+				<div class="mx-auto modal-body">
+					<div class="g-recaptcha" data-sitekey="insert sitekey here"></div>
+<!--					Below is some example text of what can be displayed when recaptcha is verified d.none is for testing purposes-->
+					<div class="text-center d-none">
+						<h4>Name</h4>
+						<p>John Doe</p>
+						<h4>Phone</h4>
+						<p>505.555.5555</p>
+						<h4>Email</h4>
+						<p>john.doe@aol.com</p>
+					</div>
 				</div>
 			</div>
 		</div>
