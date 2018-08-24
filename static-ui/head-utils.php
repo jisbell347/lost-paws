1ee926953af280dba026fa5ccc7cf499d3702cba
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-24 09:12:01 -0600

    add google reCAPTCHA api

--- static-ui/head-utils.php
+++ static-ui/head-utils.php
@@ -17,6 +17,9 @@
 	<!--font awesome-->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
 
+	<!-- Google reCAPTCHA -->
+	<script src='https://www.google.com/recaptcha/api.js'></script>
+
 	<!--google fonts-->
 	<link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,500,700" rel="stylesheet">
 
