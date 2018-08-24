a1aed25f84e1729294a787d7978edde138408545
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-24 14:57:41 -0600

    remove divs from buttons.

--- static-ui/user-profile.php
+++ static-ui/user-profile.php
@@ -58,8 +58,8 @@
 			</div><!--row-->
 			<div class="row justify-content-md-center">
 				<div class="col-md-8">
-					<button type="button" class="btn btn-primary text-light mt-3 mr-3 profile"><div class="my-btn" style="text-align:left;"><i class="fas fa-edit"></i>&nbsp;&nbsp;Update</div></button>
-					<button type="button" class="btn btn-warning text-light mt-3 profile" onclick="resetAll()"><div class="my-btn" style="text-align:left;"><i class="fas fa-undo"></i>&nbsp;&nbsp;Reset</div></button>
+					<button type="button" class="btn btn-primary text-light mt-3 mr-3 profile"><i class="fas fa-edit"></i>&nbsp;&nbsp;Update</button>
+					<button type="button" class="btn btn-warning text-light mt-3 profile" onclick="resetAll()"><i class="fas fa-undo"></i>&nbsp;&nbsp;Reset</button>
 				</div><!--col-md-8-->
 			</div><!--row-->
 		</form>
