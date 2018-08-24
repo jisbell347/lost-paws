6071feca216f77421c02ac4aaf9d1091ccbbe9d0
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-24 14:58:26 -0600

    add margin top

--- static-ui/footer.php
+++ static-ui/footer.php
@@ -1,5 +1,5 @@
 <?php require_once ("head-utils.php");?>
 
 <footer class="page-footer bg-dark text-light">
-		<div class=" text-center pt-2 pb-3">Lost-Paws ©2018</div>
+		<div class=" text-center mt-4 pt-2 pb-3">Lost-Paws ©2018</div>
 </footer>
