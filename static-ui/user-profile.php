db10f3c63e0a7b3eec7b7e23796202fbf9f6d349
Asya Nikitina <anikitina@cnm.edu>
2018-08-24 10:40:26 -0600

    Added the UI for the "update profile" view.

---
+++ static-ui/user-profile.php
@@ -0,0 +1,67 @@
+<?php require_once ("head-utils.php");?>
+
+<?php require_once("navbar.php");?>
+
+<main>
+	<div class="container">
+		<div class="row justify-content-md-center">
+			<div class="col-md-8">
+				<h1 class="text-center my-5">Update Profile</h1>
+			</div><!--col-md-8-->
+		</div><!--row-->
+		<form class="form-control-lg" id="form-profile" action="" method="post">
+			<div class="row justify-content-md-center">
+				<div class="col-md-4">
+					<label class="control-label" for="first-name">First Name <span class="red-star">*</span></label>
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
+						</div><!--input-group-prepend-->
+						<input type="text" class="form-control" id="first-name" placeholder="Your first name" name="first-name" required />
+					</div><!--		input-group mb-3-->
+				</div><!--col-md-4-->
+				<div class="col-md-4">
+					<label class="control-label" for="last-name">Last Name <span class="red-star">*</span></label>
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fa fa-user" aria-hidden="true"></i></span>
+						</div><!--input-group-prepend-->
+						<input type="text" class="form-control" id="first-name" placeholder="Your last name" name="last-name" required />
+					</div><!--		input-group mb-3-->
+				</div><!--col-md-4-->
+			</div><!--row-->
+			<div class="row justify-content-md-center">
+				<div class="col-md-8">
+					<div class="form-group mb-3">
+						<label class="control-label" for="email">Email <span class="red-star">*</span></label>
+						<div class="input-group">
+							<div class="input-group-prepend">
+								<span class="input-group-text"><i class="fa fa-envelope" aria-hidden="true"></i></span>
+							</div><!--	input-group-prepend-->
+							<input type="text" class="form-control" placeholder="Enter your email address" name="email" aria-describedby="emailHelp" required />
+						</div><!--	input-group-->
+						<small id="emailHelp" >We will never share your email address with anyone else.</small>
+					</div><!--	form-group mb-3-->
+				</div><!--col-md-8-->
+			</div><!--row-->
+			<div class="row justify-content-md-center">
+				<div class="col-md-8">
+					<label class="control-label" for="last-name">Phone Number <span class="red-star">*</span></label>
+					<div class="input-group mb-3">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fa fa-phone" aria-hidden="true"></i></span>
+						</div><!--	input-group-prepend-->
+						<input type="tel" id="phone" name="phone" class="form-control" placeholder="123-456-7890" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+						<span class="validity"></span>
+					</div><!--		input-group mb-3-->
+				</div><!--col-md-8-->
+			</div><!--row-->
+			<div class="row justify-content-md-center">
+				<div class="col-md-8">
+					<button type="button" class="btn btn-primary"><div class="my-btn" style="text-align:left;"><i class="fa fa-pencil-square-o" style="color:white;" aria-hidden="true"></i>&nbsp;&nbsp;Update</div></button>
+					<button type="button" class="btn btn-warning" onclick="resetAll()"><div class="my-btn" style="text-align:left;"><i class="fa fa-undo" style="color:white;" aria-hidden="true"></i>&nbsp;&nbsp;Reset</div></button>
+				</div><!--col-md-8-->
+			</div><!--row-->
+		</form>
+	</div><!--		container-->
+</main>
