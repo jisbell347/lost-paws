5c889731d98d2e18b031355570170d684abaf969
Asya Nikitina <anikitina@cnm.edu>
2018-08-25 20:53:39 -0600

    Corrected a few typos in user-profile.php

--- static-ui/user-profile.php
+++ static-ui/user-profile.php
@@ -18,7 +18,7 @@
 							<span class="input-group-text"><i class="fas fa-user text-primary"></i></span>
 						</div><!--input-group-prepend-->
 						<input type="text" class="form-control" id="first-name" placeholder="Your first name" name="first-name" required />
-					</div><!--		input-group mb-3-->
+					</div><!--input-group mb-3-->
 				</div><!--col-md-3-->
 				<div class="col-md-3">
 					<label class="control-label" for="last-name">Last Name <span class="text-danger">*</span></label>
@@ -26,8 +26,8 @@
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user text-primary"></i></span>
 						</div><!--input-group-prepend-->
-						<input type="text" class="form-control" id="first-name" placeholder="Your last name" name="last-name" required />
-					</div><!--		input-group mb-3-->
+						<input type="text" class="form-control" id="last-name" placeholder="Your last name" name="last-name" required />
+					</div><!--input-group mb-3-->
 				</div><!--col-md-3-->
 			</div><!--row-->
 			<div class="row justify-content-md-center">
@@ -37,16 +37,16 @@
 						<div class="input-group">
 							<div class="input-group-prepend">
 								<span class="input-group-text"><i class="fas fa-envelope text-primary"></i></span>
-							</div><!--	input-group-prepend-->
+							</div><!--input-group-prepend-->
 							<input type="text" class="form-control" placeholder="Enter your email address" name="email" aria-describedby="emailHelp" required />
 						</div><!--	input-group-->
-						<small id="emailHelp" >We will never share your email address with anyone else.</small>
+						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 					</div><!--	form-group mb-3-->
 				</div><!--col-md-6-->
 			</div><!--row-->
 			<div class="row justify-content-md-center">
 				<div class="col-md-6">
-					<label class="control-label" for="last-name">Phone Number <span class="text-danger">*</span></label>
+					<label class="control-label" for="phone">Phone Number <span class="text-danger">*</span></label>
 					<div class="input-group mb-3">
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-phone text-primary"></i></span>
