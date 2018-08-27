a897ce88c1dff4c4ad7aae41b3b82b017cc2b425
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-27 17:01:40 -0600

    Removed div tags

--- static-ui/footer.php
+++ static-ui/footer.php
@@ -1,5 +1,6 @@
 <?php require_once ("head-utils.php");?>
 
-<footer class="page-footer bg-dark text-light">
-		<div class=" text-center mt-4 pt-2 pb-3">Lost-Paws ©2018</div>
+<footer class="footer bg-dark text-light">
+			<div class=" text-center mt-4 py-2">Lost-Paws ©2018</div>
 </footer>
+
