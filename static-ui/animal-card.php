9f35664b3208a6ad3ad1fe3afda736a0ba743efb
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-28 16:25:10 -0600

    Added sfooter class and content div

--- static-ui/animal-card.php
+++ static-ui/animal-card.php
@@ -5,25 +5,27 @@
 
 
 
-<main class="pt-5">
-	<div class="container p-4">
-		<div class="row justify-content-center">
-			<h3 class="text-center">Animal Profile</h3>
-		</div>
-		<div class="card border border-0">
-			<img src="http://placekitten.com/500/500" alt="Random image of a cat" class="card-image-top p-5 img-fluid mx-auto">
-			<h4 class="text-center">About this animal:</h4>
-			<ul class="list-group">
-				<li class="list-group-item">Status: Found</li>
-				<li class="list-group-item">Species: Cat</li>
-				<li class="list-group-item">Name: Fuzzy</li>
-				<li class="list-group-item">Color: White/Brown</li>
-				<li class="list-group-item">Location Lost/Found: 1st and Main</li>
-				<li class="list-group-item">Gender: Male</li>
-				<li class="list-group-item">Description: Fuzzy is a three year-old male cat with light brown and white fur. He was last seen near our house on 1st and Main about 10pm on the 18th of August wearing a blue collar.</li>
-			</ul>
-			<h4 class="p-3 text-center mt-4">Contact:</h4>
-			<button type="button" class="btn btn-info" data-toggle="modal" data-target="#contact">Info</button>
+<main class="sfooter, pt-5">
+	<div class="sfooter-content">
+		<div class="container p-4">
+			<div class="row justify-content-center">
+				<h3 class="text-center">Animal Profile</h3>
+			</div>
+			<div class="card border border-0">
+				<img src="http://placekitten.com/500/500" alt="Random image of a cat" class="card-image-top p-5 img-fluid mx-auto">
+				<h4 class="text-center">About this animal:</h4>
+				<ul class="list-group">
+					<li class="list-group-item">Status: Found</li>
+					<li class="list-group-item">Species: Cat</li>
+					<li class="list-group-item">Name: Fuzzy</li>
+					<li class="list-group-item">Color: White/Brown</li>
+					<li class="list-group-item">Location Lost/Found: 1st and Main</li>
+					<li class="list-group-item">Gender: Male</li>
+					<li class="list-group-item">Description: Fuzzy is a three year-old male cat with light brown and white fur. He was last seen near our house on 1st and Main about 10pm on the 18th of August wearing a blue collar.</li>
+				</ul>
+				<h4 class="p-3 text-center mt-4">Contact:</h4>
+				<button type="button" class="btn btn-info" data-toggle="modal" data-target="#contact">Info</button>
+			</div>
 		</div>
 	</div>
 </main>
--- static-ui/animal-search.php
+++ static-ui/animal-search.php
@@ -1,63 +1,65 @@
 <?php require_once ("head-utils.php"); ?>
 <?php require_once ("navbar.php"); ?>
 
-<main>
-	<div class="container pt-5">
-		<div class="row justify-content-between align-items-center">
-			<h2 class="p-3">Search Results</h2>
-			<label for="sort"></label>
-			<select id="sort">
-				<option value="">--Filter By--</option>
-				<option value="color">Color</option>
-				<option value="gender">Gender</option>
-				<option value="name">Name</option>
-				<option value="species">Species</option>
-				<option value="status">Status</option>
-			</select>
-		</div>
+<main class="sfooter">
+	<div class="sfooter-content">
+		<div class="container pt-5">
+			<div class="row justify-content-between align-items-center">
+				<h2 class="p-3">Search Results</h2>
+				<label for="sort"></label>
+				<select id="sort">
+					<option value="">--Filter By--</option>
+					<option value="color">Color</option>
+					<option value="gender">Gender</option>
+					<option value="name">Name</option>
+					<option value="species">Species</option>
+					<option value="status">Status</option>
+				</select>
+			</div>
 
-		<div class="row bg-dark text-light justify-content-center rounded-top">
-			<h4>Fuzzy</h4>
+			<div class="row bg-dark text-light justify-content-center rounded-top">
+				<h4>Fuzzy</h4>
+			</div>
+			<div class="row align-items-center bg-dark rounded-bottom pb-3 mb-4">
+				<div class="col-md-4">
+					<img src="http://placekitten.com/300/250" alt="placeholder cat" class="img-fluid rounded p-3">
+				</div>
+				<div class="col-md-4">
+					<ul class="list-group">
+						<li class="list-group-item">Name: Fuzzy</li>
+						<li class="list-group-item">Status: Lost</li>
+						<li class="list-group-item">Species: Cat</li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<ul class="list-group">
+						<li class="list-group-item">Location: 1st and Main</li>
+						<li class="list-group-item">Gender: Male</li>
+						<li class="list-group-item">Color: Grey</li>
+					</ul>
+				</div>
+			</div>
 		</div>
-		<div class="row align-items-center bg-dark rounded-bottom pb-3 mb-4">
-			<div class="col-md-4">
-				<img src="http://placekitten.com/300/250" alt="placeholder cat" class="img-fluid rounded p-3">
+		<div class="container pt-5">
+			<div class="row justify-content-center">
+				<nav aria-label="...">
+					<ul class="pagination">
+						<li class="page-item disabled">
+							<a class="page-link" href="#" tabindex="-1">Previous</a>
+						</li>
+						<li class="page-item active">
+							<a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
+						</li>
+						<li class="page-item">
+							<a class="page-link" href="#">2</a>
+						</li>
+						<li class="page-item"><a class="page-link" href="#">3</a></li>
+						<li class="page-item">
+							<a class="page-link" href="#">Next</a>
+						</li>
+					</ul>
+				</nav>
 			</div>
-			<div class="col-md-4">
-				<ul class="list-group">
-					<li class="list-group-item">Name: Fuzzy</li>
-					<li class="list-group-item">Status: Lost</li>
-					<li class="list-group-item">Species: Cat</li>
-				</ul>
-			</div>
-			<div class="col-md-4">
-				<ul class="list-group">
-					<li class="list-group-item">Location: 1st and Main</li>
-					<li class="list-group-item">Gender: Male</li>
-					<li class="list-group-item">Color: Grey</li>
-				</ul>
-			</div>
-		</div>
-	</div>
-	<div class="container pt-5">
-		<div class="row justify-content-center">
-			<nav aria-label="...">
-				<ul class="pagination">
-					<li class="page-item disabled">
-						<a class="page-link" href="#" tabindex="-1">Previous</a>
-					</li>
-					<li class="page-item active">
-						<a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
-					</li>
-					<li class="page-item">
-						<a class="page-link" href="#">2</a>
-					</li>
-					<li class="page-item"><a class="page-link" href="#">3</a></li>
-					<li class="page-item">
-						<a class="page-link" href="#">Next</a>
-					</li>
-				</ul>
-			</nav>
 		</div>
 	</div>
 </main>
