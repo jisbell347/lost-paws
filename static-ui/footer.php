b3a0001c21d372070e3ac0e7ef72b8d579f33f18
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-28 14:36:37 -0600

    Moved footer into own container. See if that works better

--- static-ui/footer.php
+++ static-ui/footer.php
@@ -1,6 +1,10 @@
 <?php require_once ("head-utils.php");?>
 
-<footer class="footer bg-dark text-light">
-			<div class=" text-center mt-4 py-2">Lost-Paws ©2018</div>
+<footer class="bg-dark text-light">
+	<div class="container-fluid mt-4">
+		<div class="row justify-content-center">
+			<div class=" text-center py-2">Lost-Paws ©2018</div>
+		</div>
+	</div>
 </footer>
 
