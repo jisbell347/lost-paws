907aba92e1f0cdc52406c67378ad2e43f4bdbfa0
Asya Nikitina <anikitina@cnm.edu>
2018-08-28 14:13:32 -0600

    Rearranged the form in user-profile.php -- made it nested inside a container.

--- static-ui/user-profile.php
+++ static-ui/user-profile.php
@@ -1,15 +1,15 @@
 <?php require_once ("head-utils.php");?>
 <?php require_once("navbar.php");?>
-<?php /*require_once ("footer.php"); */?>
 
-<main class="form-padding">
-	<div class="container">
-		<div class="row justify-content-md-center">
-			<div class="col-md-6">
-				<h1 class="text-center my-5">Update Profile</h1>
-			</div><!--col-md-6-->
-		</div><!--row-->
-		<form class="form-control-lg" id="form-profile" action="" method="post">
+
+<main>
+	<form class="form-control-lg" id="form-profile" action="" method="post">
+		<div class="container">
+			<div class="row justify-content-md-center">
+				<div class="col-md-6">
+					<h1 class="text-center my-5">Update Profile</h1>
+				</div><!--col-md-6-->
+			</div><!--row justify-content-md-center-->
 			<div class="row justify-content-md-center">
 				<div class="col-md-3">
 					<label class="control-label" for="first-name">First Name <span class="text-danger">*</span></label>
@@ -29,7 +29,7 @@
 						<input type="text" class="form-control" id="last-name" placeholder="Your last name" name="last-name" required />
 					</div><!--input-group mb-3-->
 				</div><!--col-md-3-->
-			</div><!--row-->
+			</div><!--row justify-content-md-center-->
 			<div class="row justify-content-md-center">
 				<div class="col-md-6">
 					<div class="form-group mb-3">
@@ -43,7 +43,7 @@
 						<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 					</div><!--	form-group mb-3-->
 				</div><!--col-md-6-->
-			</div><!--row-->
+			</div><!--row justify-content-md-center-->
 			<div class="row justify-content-md-center">
 				<div class="col-md-6">
 					<label class="control-label" for="phone">Phone Number <span class="text-danger">*</span></label>
@@ -55,15 +55,16 @@
 						<span class="validity"></span>
 					</div><!--		input-group mb-3-->
 				</div><!--col-md-6-->
-			</div><!--row-->
+			</div><!--row justify-content-md-center-->
 			<div class="row justify-content-md-center">
 				<div class="col-md-6">
-					<button type="button" class="btn btn-primary text-light mt-3 mr-3 profile-btn"><i class="fas fa-edit"></i>&nbsp;&nbsp;Update</button>
-					<button type="button" class="btn btn-warning text-light mt-3 profile-btn"><i class="fas fa-undo"></i>&nbsp;&nbsp;Clear All</button>
+					<button type="button" class="btn btn-primary text-light mt-3 mb-5 mr-3 profile-btn"><i class="fas fa-edit"></i>&nbsp;&nbsp;Update</button>
+					<button type="button" class="btn btn-warning text-light mt-3 mb-5 profile-btn"><i class="fas fa-undo"></i>&nbsp;&nbsp;Clear All</button>
 				</div><!--col-md-6-->
-			</div><!--row-->
-		</form>
-	</div><!--		container-->
-</main>
+			</div><!--row justify-content-md-center-->
+		</div><!--		container-->
+	</form>
 
+</main>
 <?php require_once "footer.php"; ?>
+
