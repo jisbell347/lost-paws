aa3a8d20ac5cdb4e4e764488b7cfccc2c2316544
Asya Nikitina <anikitina@cnm.edu>
2018-08-29 15:33:11 -0600

    Continued working on profile.component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,10 +1,17 @@
 import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Router} from "@angular/router";
+import {ProfileService} from "./app/shared/servides/profile.service";
+import {Profile} from "../classes/Profile";
+
 
 @Component({
 	selector: "profile",
 	template: require("./profile.component.html")
 })
 export class ProfileComponent{
-	constructor() {
+	profile: Profile = {};
+
+	constructor(protected formBuilder: FormBuilder, protected profileService: ProfileService, private router: Router) {}
 	}
 }
