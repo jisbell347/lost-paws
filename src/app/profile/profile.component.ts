7cf79efc301ab7ad15ce3939428f1aa0b2e97044
Asya Nikitina <anikitina@cnm.edu>
2018-08-29 17:04:58 -0600

    Continued working on profile.component

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,17 +1,32 @@
 import {Component, OnInit} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
-import {ProfileService} from "./app/shared/servides/profile.service";
-import {Profile} from "../classes/Profile";
+import {ProfileService} from "../shared/services/profile.service";
+import {Profile} from "../shared/interfaces/profile";
 
 
 @Component({
 	selector: "profile",
 	template: require("./profile.component.html")
 })
-export class ProfileComponent{
-	profile: Profile = {};
+export class ProfileComponent implements OnInit {
+	profile: Profile;
+	profileForm: FormGroup;
+
 
 	constructor(protected formBuilder: FormBuilder, protected profileService: ProfileService, private router: Router) {}
+
+	// need to grab profileId from the current Session
+	// the remaining fields - from the form
+
+	ngOnInit() : void {
+		this.profileForm = this.formBuilder.group({
+			first-name: ["", [Validators.maxLength(46), Validators.required]],
+
+		});
+
 	}
+
+
+
 }
