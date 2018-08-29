a3f9a7e6a937458c10fa2aa8053faad29ea41613
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-29 15:01:46 -0600

    Added app component decorator

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from "@angular/core";
+import {Status} from "./shared/interfaces/status";
+import {SessionService} from "./shared/services/session.service";
+
+@Component({
+	selector: "lost-paws",
+	template: require("./app.component.html")
+})
+export class AppComponent {
+	constructor() {
+
+	}
+}
