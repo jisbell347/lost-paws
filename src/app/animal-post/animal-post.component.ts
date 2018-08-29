94075af3181380aa14c176d212ce66896c146d1c
Asya Nikitina <anikitina@cnm.edu>
2018-08-29 14:46:03 -0600

    Added content to the animal-post.component.ts and animal-post.component.html

---
+++ src/app/animal-post/animal-post.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+	selector: "animal-post",
+	template: require("./animal-post.component.html")
+})
+export class AnimalPostComponent{
+
+	constructor() {
+	}
+}
