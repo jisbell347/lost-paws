2342d1ba8c1181f598190192a399c2341eb7f9da
Asya Nikitina <anikitina@cnm.edu>
2018-08-29 15:21:10 -0600

    Started working on profile.component

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from "@angular/core";
+
+@Component({
+	selector: "profile",
+	template: require("./profile.component.html")
+})
+export class ProfileComponent{
+	constructor() {
+	}
+}
