e480945d4a5cd8392d9e197aef2be28ccc582614
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-30 09:59:18 -0600

    Added animal card component

---
+++ src/app/animal/animal.card.component.ts
@@ -0,0 +1,31 @@
+import {Component, OnInit} from "@angular/core";
+import {ActivatedRoute, Router} from "@angular/router";
+import {AnimalService} from "../shared/services/animal.service";
+import {Animal} from "../shared/interfaces/animal";
+import {Observable} from "rxjs";
+
+@Component({
+	selector: "animal-card",
+	template: require("./animal.card.template.html")
+
+})
+
+export class AnimalCardComponent implements OnInit{
+	animal: Animal;
+
+
+	constructor(protected animalService: AnimalService, protected router: ActivatedRoute) {
+
+	}
+	animalId = this.router.snapshot.params["animalId"];
+	ngOnInit() {
+		this.loadAnimal();
+	}
+
+	loadAnimal() {
+		this.animalService.getAnimal(this.animalId).subscribe(reply => {
+			this.animal = reply;
+		});
+	}
+
+}
