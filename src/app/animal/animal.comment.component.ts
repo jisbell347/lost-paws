ec01e4de233bec5f2aa533c7f10d5960cd06507a
Joseph Isbell <shadowwolf347@comcast.net>
2018-08-30 14:04:15 -0600

    Added html and component decorator for comments. needs work still

---
+++ src/app/animal/animal.comment.component.ts
@@ -0,0 +1,76 @@
+import {Component, OnInit} from "@angular/core";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
+//Interfaces used by the component
+import {Comment} from "../shared/interfaces/comment";
+import {Animal} from "../shared/interfaces/animal";
+import {Profile} from "../shared/interfaces/profile";
+
+// Services needed
+
+import {AnimalService} from "../shared/services/animal.service";
+import {ProfileService} from "../shared/services/profile.service";
+import {CommentService} from "../shared/services/comment.service";
+
+//Status and router
+import {Status} from "../shared/interfaces/status";
+import {ActivatedRoute, Params} from "@angular/router";
+
+@Component({
+	template: require("./animal.comment.template.html"),
+	selector: "comment"
+})
+
+export class AnimalCommentComponent implements OnInit {
+	animal: Animal;
+	comment: Comment;
+	comments: Comment[] = [];
+	animalId = this.route.snapshot.params["animalId"];
+	createCommentForm: FormGroup;
+	status: Status;
+
+	constructor(
+		protected formBuilder: FormBuilder,
+		protected commentService: CommentService,
+		protected animalService: AnimalService,
+		protected route: ActivatedRoute
+	) {
+	}
+
+	ngOnInit(): void {
+		this.animalService.getAnimal(this.animalId).subscribe(reply => this.animal = reply);
+		this.loadComments();
+		this.createCommentForm = this.formBuilder.group({
+			commentText: ["", [Validators.maxLength(1000), Validators.required]]
+		});
+
+	}
+
+	loadComments() : any {
+		this.commentService.getCommentbyAnimalId(this.animalId).subscribe(comments => this.comments = comments);
+	}
+
+	createAnimalComment(): any {
+		let comment: Comment = {
+			commentId: null,
+			commentAnimalId: null,
+			commentProfileId: null,
+			commentDate: null,
+			commentText: this.createCommentForm.value.commentText
+		};
+
+		this.commentService.createComment(comment)
+			.subscribe(status => {
+				this.status = status;
+
+				if(this.status.status == 200) {
+					this.loadComments();
+					this.createCommentForm.reset();
+				} else {
+					return false
+				}
+			});
+	}
+
+
+}
