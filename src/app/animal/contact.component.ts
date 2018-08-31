c80c7ca9430ad3d4286b051782cdb9b8a90625da
Jude Baca-Miller <judeamiller@gmail.com>
2018-08-31 13:53:13 -0600

    begin adding code to export class to get profile information to the contact modal.

--- src/app/animal/contact.component.ts
+++ src/app/animal/contact.component.ts
@@ -1,5 +1,8 @@
 import {Component, ViewChild, OnInit} from "@angular/core";
-import {Router} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
+import {ProfileService} from "../shared/services/profile.service";
+import  {Profile} from "../shared/interfaces/profile";
+import {Observable} from "rxjs";
 
 
 @Component({
@@ -7,6 +10,22 @@ import {Router} from "@angular/router";
 	selector: "contact"
 })
 
-export class ContactComponent {
+export class ContactComponent  implements OnInit{
+	profile: Profile;
+
+	constructor(protected profileService: ProfileService, protected router: ActivatedRoute) {
+
+	}
+
+	profileId = this.router.snapshot.params["profileId"];
+	ngOnInit() {
+		this.loadProfile();
+	}
+
+	loadProfile() {
+		this.profileService.getProfile(this.profileId).subscribe( reply => {
+			this.profile = reply;
+		})
+	}
 }
 
