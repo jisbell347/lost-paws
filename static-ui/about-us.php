7f4f9bd48d10f1f68539b7fddc1892590171e4be
Jude Baca-Miller <judeamiller@gmail.com>
2018-09-01 12:44:07 -0600

    initial commit of the about-us static ui

---
+++ static-ui/about-us.php
@@ -0,0 +1,25 @@
+<?php require_once ("head-utils.php");?>
+<?php require_once("navbar.php");?>
+
+<main>
+	<div class="container pt-5">
+		<h2>About Lost-Paws</h2>
+		<p>Lost Paws is home to a community of pet lovers dedicated to helping lost pets reunite with their families. Lost Paws
+		was created by a team of four web developers enrolled in the 21st cohort of the Deep Dive Coding bootcamp. We created
+		Lost Paws because we believe that your pets are family, and if you ever loose your pet it should not be seen as a lost cause.</p>
+
+		<h2>Who are we?</h2>
+		<div class="row text-center">
+			<div class="col-lg-2 col-sm-1">
+			</div>
+		</div>
+		<div class="row text-center">
+			<div class="col-lg-2 col-sm-1">
+			</div>
+		</div>
+
+
+	</div>
+</main>
+
+<?php require_once ("footer.php");?>
