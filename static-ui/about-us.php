6ad19a2eabaf1291cfa25965dbe8bd67adedd33d
Jude Baca-Miller <judeamiller@gmail.com>
2018-09-01 18:59:17 -0600

    add about-us page to static ui. Used github profile images as placeholder.  Lorem ipsum as biography placeholder.

--- static-ui/about-us.php
+++ static-ui/about-us.php
@@ -8,14 +8,29 @@
 		was created by a team of four web developers enrolled in the 21st cohort of the Deep Dive Coding bootcamp. We created
 		Lost Paws because we believe that your pets are family, and if you ever loose your pet it should not be seen as a lost cause.</p>
 
-		<h2>Who are we?</h2>
+		<h2 class="pt-3">Development Team</h2>
 		<div class="row text-center">
-			<div class="col-lg-2 col-sm-1">
+			<div class="col-lg-6 col-sm-12 pb-5">
+				<img src="./images/about-adel.jpg" alt="photo of Adel" class="rounded border border-secondary shadow-lg mx-auto mb-3 mt-4 about-picture"/>
+				<h4 class="text-secondary">Adel Moreno</h4>
+				<p>Adel... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
 			</div>
+			<div class="col-lg-6 col-sm-12 pb-5">
+				<img src="./images/about-asya.jpg" alt="photo of Asya" class="rounded border border-secondary shadow-lg mx-auto mb-3 mt-4 about-picture"/>
+				<h4 class="text-secondary">Asya Nikitina</h4>
+				<p>Asya... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
 		</div>
 		<div class="row text-center">
-			<div class="col-lg-2 col-sm-1">
-			</div>
+			<div class="col-lg-6 col-sm-12 pb-5">
+				<img src="./images/about-joe.jpg" alt="photo of Joe" class="rounded border border-secondary shadow-lg mx-auto mb-3 mt-4 about-picture"/>
+				<h4 class="text-secondary">Joeseph Isbell </h4>
+				<p>Joe... Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+		</div>
+		<div class="col-lg-6 col-sm-12 pb-5">
+			<img src="./images/about-jude.jpg" alt="photo of Jude" class="rounded border border-secondary shadow-lg mx-auto mb-3 mt-4 about-picture"/>
+			<h4 class="text-secondary">Jude Baca-Miller</h4>
+			<p>Jude is a web developer from Albuquerque who enjoys problem solving and creating pleasing user experiences. Jude shares a house with his wife, two elderly cats and a rambunctious terrier.</p>
 		</div>
 
 
