59af323d5e4624d52e08f3a4c023e0f46dfc5481
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-04 14:42:49 -0600

    Changed navbar to determine if user is signed in or not

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,9 +1,23 @@
-import{Component} from "@angular/core";
+import{Component, OnInit} from "@angular/core";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
 	template: require("./navbar.template.html"),
 	selector: "navbar"
 })
 
-export class NavbarComponent{
+export class NavbarComponent implements OnInit{
+
+	isAuthenticated: boolean;
+
+	constructor(protected authService: AuthService) {
+
+	}
+
+	ngOnInit() {
+		this.isAuthenticated = this.authService.loggedIn();
+	}
+
+
 }
+
