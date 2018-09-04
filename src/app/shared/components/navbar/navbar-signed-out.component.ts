18a4a5f3ccaa48bf0b5ca2e54fad6565110dc6be
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-04 13:49:23 -0600

    Deleted signed out navbar

--- src/app/shared/components/navbar/navbar-signed-out.component.ts
+++
@@ -1,9 +0,0 @@
-import{Component} from "@angular/core";
-
-@Component({
-	template: require("./navbar-signed-out.template.html"),
-	selector: "navbar-signed-out"
-})
-
-export class NavbarComponentSignedOut{
-}
