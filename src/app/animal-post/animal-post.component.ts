557f46de4e63a363380e2ba399720de8178bb86d
Asya Nikitina <anikitina@cnm.edu>
2018-09-04 15:22:17 -0600

    Renamed animal-post.template.html file

--- src/app/animal-post/animal-post.component.ts
+++ src/app/animal-post/animal-post.component.ts
@@ -2,7 +2,7 @@ import {Component, OnInit} from "@angular/core";
 
 @Component({
 	selector: "animal-post",
-	template: require("./animal-post.component.html")
+	template: require("./animal-post.template.html")
 })
 export class AnimalPostComponent{
 	constructor() {
