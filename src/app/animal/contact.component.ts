8e5ddd74ce2668e98c4c4c853a5ba069408a85da
Jude Baca-Miller <judeamiller@gmail.com>
2018-09-04 14:30:45 -0600

    commented out poor attempts at getting profile info to display. now displays profile id. Its a  start.

--- src/app/animal/contact.component.ts
+++ src/app/animal/contact.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from "@angular/core";
+import {Observable} from "rxjs";
 
 //Interfaces
 import  {Profile} from "../shared/interfaces/profile";
@@ -9,43 +10,52 @@ import {ProfileService} from "../shared/services/profile.service";
 import {AnimalService} from "../shared/services/animal.service";
 
 //Router
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {Status} from "../shared/interfaces/status";
 
 
 @Component({
-	template: require("./contact.template.html"),
-	selector: "contact"
-})
+	selector: "contact",
+	template: require("./contact.template.html")
+	})
 
 export class ContactComponent  implements OnInit{
 	animal: Animal;
-	animalId = this.route.snapshot.params["animalId"];
-	animalProfileId = this.route.snapshot["animalProfileId"];
-	profile: Profile;
-	profileId = this.route.snapshot.params["profileId"];
-	status: Status = {status: null, message: null, type: null};
+	// animalId = this.route.snapshot.params["animalId"];
+	// animalProfileId = this.route.snapshot["animalProfileId"];
+	// profile: Profile;
+	// profileId = this.route.snapshot.params["profileId"];
+	// status: Status = {status: null, message: null, type: null};
 
 	constructor(
 		protected animalService: AnimalService,
-		protected profileService: ProfileService,
-		protected route: ActivatedRoute
+		// protected profileService: ProfileService,
+		protected router: ActivatedRoute
 	) {
 
 	}
 
+	animalId= this.router.snapshot.params["animalId"];
 
 	ngOnInit() {
-		this.loadProfile();
-		this.animalService.getAnimal(this.animalId).subscribe(profileId => this.animal = profileId);
+		this.loadAnimal();
+
+
+		// this.loadProfile();
+		// this.animalService.getAnimal(this.animalId).	subscribe(profileId => this.animal = profileId);
 	}
 
-	loadProfile() {
-		this.profileService.getProfile(this.profileId).subscribe( profile => {
-			this.profile = profile;
-		})
-
-
+	loadAnimal() {
+		this.animalService.getAnimal(this.animalId).subscribe(reply => {
+			this.animal = reply;
+		});
 	}
+	// loadProfile() {
+	// 	this.profileService.getProfile(this.profileId).subscribe( profile => {
+	// 		this.profile = profile;
+	// 	})
+	//
+	//
+	// }
 }
 
