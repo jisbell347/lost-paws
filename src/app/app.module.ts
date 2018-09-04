ea9f26ed002c3244c97d2f548b4f9661b45f4cc6
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-04 13:45:55 -0600

    Added JwtHelper

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,26 @@ import {HttpClientModule} from "@angular/common/http";
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {JwtModule} from "@auth0/angular-jwt";
 
 
 const moduleDeclarations = [AppComponent];
 
+//configure the parameters fot the JwtModule
+const JwtHelper = JwtModule.forRoot({
+	config: {
+		tokenGetter: () => {
+			return localStorage.getItem("jwt-token");
+		},
+		skipWhenExpired:true,
+		whitelistedDomains: ["localhost:7272", "https://bootcamp-coders.cnm.edu/"],
+		headerName:"X-JWT-TOKEN",
+		authScheme: ""
+	}
+});
+
 @NgModule({
-	imports: 		[BrowserModule, HttpClientModule, ReactiveFormsModule, FormsModule, routing],
+	imports: 		[BrowserModule, HttpClientModule, JwtHelper, ReactiveFormsModule, FormsModule, routing],
 	declarations:  [...moduleDeclarations, ...allAppComponents],
 	bootstrap:		[AppComponent],
 	providers:		[appRoutingProviders]
