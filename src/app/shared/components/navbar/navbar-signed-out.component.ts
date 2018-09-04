e14daa03ca21480fcd1c5b7f80a46476181131d1
Jude Baca-Miller <judeamiller@gmail.com>
2018-09-04 09:07:21 -0600

    initial commit of signed-out navbar component

---
+++ src/app/shared/components/navbar/navbar-signed-out.component.ts
@@ -0,0 +1,9 @@
+import{Component} from "@angular/core";
+
+@Component({
+	template: require("./navbar-signed-out.template.html"),
+	selector: "navbar-signed-out"
+})
+
+export class NavbarComponentSignedOut{
+}
