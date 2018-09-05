f5b3e19c99ce99abb2c80434cf5f32762f9352ce
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-05 09:57:33 -0600

    Added service, route and oninit

--- src/app/shared/components/navbar/google.exit.component.ts
+++ src/app/shared/components/navbar/google.exit.component.ts
@@ -1,10 +1,28 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {GoogleExitService} from "../../services/google.exit.service";
+import {ActivatedRoute, Router} from "@angular/router";
 
+import {Status} from "../../interfaces/status";
 
 @Component({
 	template: require("./google.exit.template.html")
 })
 
-export class GoogleExitComponent {
+export class GoogleExitComponent implements OnInit{
+	code: string = this.route.snapshot.queryParams["code"];
+	status: Status = null;
 
+	constructor(protected googleExitService: GoogleExitService, protected route: ActivatedRoute, protected router: Router){
+
+	}
+
+	ngOnInit(){
+		this.googleExitService.getRedirect(this.code).subscribe(status => {
+			this.status = status;
+
+			if(this.status.status === 200) {
+				this.router.navigate([""]);
+			}
+		});
+	}
 }
