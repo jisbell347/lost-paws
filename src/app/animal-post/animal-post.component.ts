462218aef3800de2b1616aa2be166b3e5047c718
Asya Nikitina <anikitina@cnm.edu>
2018-09-05 10:29:42 -0600

    Completed writing the post an animal component and template for it.

--- src/app/animal-post/animal-post.component.ts
+++ src/app/animal-post/animal-post.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild } from "@angular/core";
-import { NgForm } from '@angular/forms';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, FormControl,  ReactiveFormsModule, Validators } from '@angular/forms';
 import { Animal } from "../shared/interfaces/animal";
 import { AnimalService } from "../shared/services/animal.service";
 import { AuthService } from "../shared/services/auth.service";
@@ -10,16 +10,61 @@ import {Status} from "../shared/interfaces/status";
 	template: require("./animal-post.template.html")
 })
 export class AnimalPostComponent implements OnInit {
-	@ViewChild('f2') animalForm: NgForm;
+	animalForm: FormGroup;
+	profileId: string;
+	animal: Animal;
+	submitted = false;
+	status : Status = null;
 
-
-
-	constructor() {
+	constructor(protected authService: AuthService, protected animalService: AnimalService, private fb: FormBuilder) {
 	}
 
-	ngOnInit() : void {}
+	ngOnInit() : void {
 
-	loadAnimal() {}
+		//grab the current logged in profileId off JWT
+		this.profileId = this.getJwtProfileId();
+		this.animalForm = this.fb.group({
+			astatus: ["", [Validators.required]],
+			aspecies: ["", [Validators.required]],
+			agender: ["", [Validators.required]],
+			aname: ["", [Validators.maxLength(100)]],
+			acolor: ["", [Validators.maxLength(25)]],
+			alocation: ["", [Validators.maxLength(200)]],
+			adescription: ["", [Validators.maxLength(500), Validators.required]],
+		});
+	}
 
-	onSubmit() : void {}
+	getJwtProfileId() : any {
+		if(this.authService.decodeJwt()) {
+			return this.authService.decodeJwt().auth.profileId;
+		} else {
+			return false
+		}
+	}
+
+	createAnAnimal(id: string) : void {
+		this.animal.animalId = null;
+		this.animal.animalProfileId = id;
+		this.animal.animalColor = this.animalForm.value.acolor;
+		this.animal.animalDate = null;
+		this.animal.animalDescription = this.animalForm.value.adesription;
+		this.animal.animalGender = this.animalForm.value.agender;
+		this.animal.animalImageUrl = null;
+		this.animal.animalLocation = this.animalForm.value.alocation;
+		this.animal.animalName = this.animalForm.value.aname;
+		this.animal.animalSpecies = this.animalForm.value.aspecies;
+		this.animal.animalStatus = this.animalForm.value.astatus;
+	}
+
+	onSubmit() : void {
+		this.submitted = true;
+		if (this.profileId) {
+			this.createAnAnimal(this.profileId);
+		}
+
+		this.animalService.createAnimal(this.animal).subscribe(status => {this.status = status;
+			if (this.status.status === 200 ) {
+				this.animalForm.reset();
+			}});
+	}
 }
