d4a59bcdd9783ead1400a8f0b1b49c1d19e16fcc
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-05 09:57:59 -0600

    Changed loggedIn to isAuthenticated

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -19,7 +19,7 @@ export class NavbarComponent implements OnInit{
 	}
 
 	ngOnInit() {
-		this.isAuthenticated = this.authService.loggedIn();
+		this.isAuthenticated = this.authService.isAuthenticated();
 	}
 
 	signOut() : void {
