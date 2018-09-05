63a05592e6d17a26367fe7295c158f6c2fcd3364
Asya Nikitina <anikitina@cnm.edu>
2018-09-05 14:41:37 -0600

    Removed all unnecessary code from the profile component (profile.component.ts).

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from '@angular/forms';
 import { Profile } from "../shared/interfaces/profile";
 import { ProfileService } from "../shared/services/profile.service";
-import { AuthService } from "../shared/services/auth.service";
 import {Status} from "../shared/interfaces/status";
 
 @Component({
@@ -11,46 +10,16 @@ import {Status} from "../shared/interfaces/status";
 })
 export class ProfileComponent implements OnInit {
 	@ViewChild('f1') userForm: NgForm;
-	profile: Profile;
-	profileId: string;
+	profile: Profile = null;
 	submitted = false;
 	status : Status = null;
 
 	// need to grab profileId from the current Session
 	// the remaining fields - from the form
-	constructor(protected authService: AuthService, protected profileService: ProfileService) {
+	constructor(protected profileService: ProfileService) {
 	}
 
 	ngOnInit() : void {
-		//grab the current logged in profileId off JWT
-		this.profileId = this.getJwtProfileId();
-		if (this.profile) {
-			console.log(this.profile);
-		}
-		else {
-			console.log("didn't get a profile ID from JWT token");
-		}
-
-		this.loadProfile();
-		console.log(this.profile ? "profile exists"  : "profile was not created" );
-	}
-
-	loadProfile() {
-		if (this.profileId) {
-			this.profileService.getProfile(this.profileId).subscribe( reply => {
-				this.profile = reply;
-			});
-
-			console.log(this.profile);
-		}
-	}
-
-	getJwtProfileId() : any {
-		if(this.authService.decodeJwt()) {
-			return this.authService.decodeJwt().auth.profileId;
-		} else {
-			return false
-		}
 	}
 
 	onSubmit() : void {
@@ -61,7 +30,6 @@ export class ProfileComponent implements OnInit {
 
 		this.profileService.editProfile(this.profile).subscribe(status => {this.status = status;
 		if (this.status.status === 200 ) {
-			console.log(this.status.message);
 			this.userForm.reset();
 		}});
 	}
