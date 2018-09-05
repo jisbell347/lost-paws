ddf1e316dc19dc5270d6fd23b4df5cda487f18c9
Jude Baca-Miller <judeamiller@gmail.com>
2018-09-05 10:36:12 -0600

    begin adding recaptcha to contact component. Ad ng if statements to hide contact info when not authenticated(signed in)

--- src/app/animal/contact.component.ts
+++ src/app/animal/contact.component.ts
@@ -6,6 +6,7 @@ import  {Profile} from "../shared/interfaces/profile";
 
 //Services
 import {ProfileService} from "../shared/services/profile.service";
+import {AuthService} from "../shared/services/auth.service";
 
 @Component({
 	selector: "contact",
@@ -15,15 +16,23 @@ import {ProfileService} from "../shared/services/profile.service";
 export class ContactComponent  implements OnInit{
 	@Input() animalProfileId: string;
 	profile: Profile = null;
+	isAuthenticated: boolean;
+	resolved(captchaResponse: string) {
+		console.log(`Resolved captcha with response ${captchaResponse}:`);
+	}
 
 
 	constructor(
 		protected profileService: ProfileService,
+		protected authService: AuthService
 	) {
 	};
 
 	ngOnInit() {
 		this.profileService.getProfile(this.animalProfileId) . subscribe(profile => this.profile = profile);
+		this.isAuthenticated = this.authService.isAuthenticated();
 	};
+
+
 }
 
