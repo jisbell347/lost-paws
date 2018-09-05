61afd1bb317c5a35bb42a40c68f4bcdc96e5d7b9
Adel Moreno <adel.argenix@gmail.com>
2018-09-05 10:54:36 -0600

    search kind of works... need to fix it so it console logs and filters by type

--- src/app/search/animal.search.component.ts
+++ src/app/search/animal.search.component.ts
@@ -1,9 +1,10 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 
 import {AnimalService} from "../shared/services/animal.service";
 import {Status} from "../shared/interfaces/status";
 import {Animal} from "../shared/interfaces/animal";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 
 @Component({
 	template: require("./animal.search.template.html"),
@@ -13,40 +14,43 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 export class AnimalSearchComponent implements OnInit{
 	animals: Animal[] = [];
 	searchForm : FormGroup;
+
 	status: Status = {status: null, message: null, type: null};
-	searchParametersColor : any[] = [
+	searchParameters : any[] = [
 		{"parameter" : "color"},
-		// {"parameter" : "gender"},
-		// {"parameter" : "description"},
-		// {"parameter" : "species"},
-		// {"parameter" : "status"},
+		{"parameter" : "gender"},
+		{"parameter" : "description"},
+		{"parameter" : "species"},
+		{"parameter" : "status"},
 	];
-	searchParametersGender : any[] = [
-		{"parameter" : "gender",},
-	];
-	searchParametersDescription : any[] = [
-		{"parameter" : "description",},
-	];
-	searchParametersSpecies : any[] = [
-		{"parameter" : "species",},
-	];
-searchParametersStatus : any[] = [
-	{"parameter" : "status",},
-];
+
 
 	constructor(protected animalService : AnimalService, protected formBuilder: FormBuilder) {
 
 	}
 
 	ngOnInit() {
-		//this.loadSearchResults();
 		this.searchForm = this.formBuilder.group({
-			searchContent: ["", [Validators.maxLength(64)]]
+			searchContent: ["", [Validators.maxLength(64), Validators.required]],
+			searchParameter: ["", [Validators.required]]
+
+
 		})
 	}
 
 	loadSearchResults() {
-		this.animalService.getAllCurrentAnimals().subscribe(animals => this.animals = animals)
+		// make an api to the animal API using any parameter
+
+		this.animalService.getAnimalByAnimalColor(this.animalColor).subscribe(animals => this.animals = animals);
+
+		// console.log the result
+
+	console.log(this.animals);
+
+		// display the results on the dom
+
+
+
 	}
 
 
