598c4e409bc1f8e9fd6a1f4b97323f7399e10352
Asya Nikitina <anikitina@cnm.edu>
2018-09-05 16:19:37 -0600

    Refactored the code in the profile component (remove all debugging messages) and tested -- everything works!

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from "@angular/core";
 import { NgForm } from '@angular/forms';
 import { Profile } from "../shared/interfaces/profile";
 import { ProfileService } from "../shared/services/profile.service";
+import { AuthService } from "../shared/services/auth.service";
 import {Status} from "../shared/interfaces/status";
 
 @Component({
@@ -11,24 +12,46 @@ import {Status} from "../shared/interfaces/status";
 export class ProfileComponent implements OnInit {
 	@ViewChild('f1') userForm: NgForm;
 	profile: Profile = null;
+	profileId: string;
 	submitted = false;
 	status : Status = null;
 
 	// need to grab profileId from the current Session
 	// the remaining fields - from the form
-	constructor(protected profileService: ProfileService) {
+	constructor(protected authService: AuthService, protected profileService: ProfileService) {
 	}
 
 	ngOnInit() : void {
+		//grab the current logged in profileId off JWT
+		this.profileId = this.getJwtProfileId();
+		this.loadProfile();
+	}
+
+	loadProfile() {
+		if (this.profileId) {
+			this.profileService.getProfile(this.profileId).subscribe( reply => {
+				this.profile = reply;
+			});
+		}
+	}
+
+	getJwtProfileId() : any {
+		if(this.authService.decodeJwt()) {
+			return this.authService.decodeJwt().auth.profileId;
+		} else {
+			return false;
+		}
 	}
 
 	onSubmit() : void {
 		this.submitted = true;
+		this.profile.profileId = this.profileId;
 		this.profile.profileName = this.userForm.value.first + " " + this.userForm.value.last;
 		this.profile.profileEmail = this.userForm.value.email;
 		this.profile.profilePhone = this.userForm.value.phone;
 
-		this.profileService.editProfile(this.profile).subscribe(status => {this.status = status;
+		this.profileService.editProfile(this.profile).subscribe(reply => {
+			this.status = reply;
 		if (this.status.status === 200 ) {
 			this.userForm.reset();
 		}});
