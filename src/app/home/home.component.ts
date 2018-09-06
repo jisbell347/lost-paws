eaecd03de57ca53e2d59e5bebafe8d1bf255b122
Jude Baca-Miller <judeamiller@gmail.com>
2018-09-06 15:05:02 -0600

    add requirements for auth service.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import {Component} from "@angular/core";
+import {AuthService} from "../shared/services/auth.service";
+import {ProfileService} from "../shared/services/profile.service";
 
 
 @Component ({
@@ -7,5 +9,16 @@ import {Component} from "@angular/core";
 })
 
 export class HomeComponent {
+	isAuthenticated: boolean;
+
+	constructor(
+		protected authService: AuthService
+	) {
+	};
+
+	ngOnInit() {
+		this.isAuthenticated = this.authService.isAuthenticated();
+	};
 
 }
+
