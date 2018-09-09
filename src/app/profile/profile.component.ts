d23ac556c3092b186975185b52179e53ded8c969
Asya Nikitina <anikitina@cnm.edu>
2018-09-08 19:07:42 -0600

    Added back the verbose debugging mode to profile.component.ts to quickly identify the stalled cache problem.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -24,6 +24,7 @@ export class ProfileComponent implements OnInit {
 	ngOnInit() : void {
 		//grab the current logged in profileId off JWT
 		this.profileId = this.getJwtProfileId();
+		/*console.log(this.profileId);*/
 		this.loadProfile();
 	}
 
@@ -49,6 +50,11 @@ export class ProfileComponent implements OnInit {
 		this.profile.profileName = this.userForm.value.first + " " + this.userForm.value.last;
 		this.profile.profileEmail = this.userForm.value.email;
 		this.profile.profilePhone = this.userForm.value.phone;
+
+/*		console.log(this.profile.profileId);
+		console.log(this.profile.profileName);
+		console.log(this.profile.profileEmail);
+		console.log(this.profile.profilePhone);*/
 
 		this.profileService.editProfile(this.profile).subscribe(reply => {
 			this.status = reply;
