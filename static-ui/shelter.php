caee3185587224c74ea4e9d2eb7238904e283e9e
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-09 17:51:47 -0600

    Added shelter info static ui page and photo

---
+++ static-ui/shelter.php
@@ -0,0 +1,63 @@
+<?php require_once("head-utils.php"); ?>
+<?php require_once("navbar.php"); ?>
+
+<main>
+	<div class="container pt-5">
+		<div class="row justify-content-center text-center">
+			<h2>Shelter Info</h2>
+		</div>
+		<div class="row justify-content-center">
+			<img class="img-fluid" src="images/shelter.jpg" alt="Image of pet shelter in ABQ"/>
+			<p>Our mission at Lost Paws is to reunite pets with their owners as quickly as possible. In order to accomplish
+				this, we provide owners and finders with
+				resources such as local shelters in order for them to locate their pets.</p>
+		</div>
+		<div class="row justify-content-center text-center">
+			<a href="https://www.cabq.gov/pets" class="btn btn-info">Visit Official Site</a>
+		</div>
+		<hr/>
+		<div class="row justify-content-center py-3">
+			<h3>Eastside Shelter Locations & Hours</h3>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<h5>City Of Albuquerque Animal Welfare Department</h5>
+				<ul class="list-group">
+					<li class="list-group-item"><strong>Address:</strong> 8920 Lomas Blvd NE, Albuquerque, NM 87112</li>
+					<li class="list-group-item"><strong>Phone:</strong> (505) 768-1975</li>
+					<li class="list-group-item"><strong>Hours:</strong> Tues-Sun 9:30-6</li>
+				</ul>
+			</div>
+			<div class="col-md-6">
+				<h5>Animal Humane New Mexico</h5>
+				<ul class="list-group">
+					<li class="list-group-item"><strong>Address:</strong> 615 Virginia St SE, Albuquerque, NM 87108</li>
+					<li class="list-group-item"><strong>Phone:</strong> (505) 323-7387</li>
+					<li class="list-group-item"><strong>Hours:</strong> Thurs-Tues 10-6</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row justify-content-center py-3 mt-5">
+			<h3>Westside Shelter Locations & Hours</h3>
+		</div>
+		<div class="row">
+			<div class="col-md-6">
+				<h5>City Of Albuquerque Animal Welfare Department</h5>
+				<ul class="list-group">
+					<li class="list-group-item"><strong>Address:</strong> 11800 Sunset Gardens Rd SW, Albuquerque, NM 87121
+					</li>
+					<li class="list-group-item"><strong>Phone:</strong> (505) 768-1975</li>
+					<li class="list-group-item"><strong>Hours:</strong> Tues-Sun 9:30-6</li>
+				</ul>
+			</div>
+			<div class="col-md-6">
+				<h5>Animal Humane New Mexico Westside Adoption Center</h5>
+				<ul class="list-group">
+					<li class="list-group-item"><strong>Address:</strong> 10141 Coors Blvd NW, Albuquerque, NM 87114</li>
+					<li class="list-group-item"><strong>Phone:</strong> (505) 255-5523</li>
+					<li class="list-group-item"><strong>Hours:</strong> Thurs-Tues 10-6</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</main>
