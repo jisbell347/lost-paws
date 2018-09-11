57dac893facb97372c042530dfc7b6a14050ee55
Jude Baca-Miller <judeamiller@gmail.com>
2018-09-11 09:56:24 -0600

    add methods to route user back to the animal page if on an animal url. Else, the user will be redirected home.

--- src/app/shared/components/navbar/google.exit.component.ts
+++ src/app/shared/components/navbar/google.exit.component.ts
@@ -3,6 +3,7 @@ import {GoogleExitService} from "../../services/google.exit.service";
 import {ActivatedRoute, Router} from "@angular/router";
 
 import {Status} from "../../interfaces/status";
+import {stringify} from "querystring";
 
 @Component({
 	template: require("./google.exit.template.html")
@@ -15,16 +16,26 @@ export class GoogleExitComponent implements OnInit{
 
 	constructor(protected googleExitService: GoogleExitService, protected route: ActivatedRoute, protected router: Router){
 
+
 	}
 
 	ngOnInit(){
+		// get return url from session storage
+		let returnUrl = window.sessionStorage.getItem('url');
+
 		this.googleExitService.getRedirect(this.code).subscribe(status => {
 			this.status = status;
 
-
 			if(this.status.status === 200) {
 				this.isAuthenticatedEvent.emit(true);
-				this.router.navigate([""]);
+
+				// navigate to the stored return url
+				if (window.sessionStorage.getItem("url").includes("animal")) {
+					window.location.href=returnUrl;
+				//if user is not on an animal card page, the user will be redirected home.
+				} else {
+					this.router.navigate([""]);
+				}
 			}
 		});
 	}
