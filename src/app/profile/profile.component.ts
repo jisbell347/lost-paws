333c81d31dd32b85587a3ffa3d779a25766571ea
Joseph Isbell <shadowwolf347@comcast.net>
2018-09-16 13:22:04 -0600

    Added function to profile to display pets that user has posted.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -5,6 +5,8 @@ import { ProfileService } from "../shared/services/profile.service";
 import { AuthService } from "../shared/services/auth.service";
 import {Status} from "../shared/interfaces/status";
 import {ActivatedRoute} from "@angular/router";
+import {Animal} from "../shared/interfaces/animal";
+import {AnimalService} from "../shared/services/animal.service";
 
 @Component({
 	selector: "profile",
@@ -16,12 +18,14 @@ export class ProfileComponent implements OnInit {
 	profileId: string;
 	submitted = false;
 	status : Status = null;
+	animals: Animal[] = [];
 
 	// need to grab profileId from the current Session
 	// the remaining fields - from the form
 	constructor(
 		protected authService: AuthService,
 		protected profileService: ProfileService,
+		protected animalService: AnimalService,
 		protected route: ActivatedRoute) {
 	}
 
@@ -32,6 +36,7 @@ export class ProfileComponent implements OnInit {
 		this.profileId = this.getJwtProfileId();
 		/*console.log(this.profileId);*/
 		this.loadProfile();
+		this.getAnimalbyProfileId();
 	}
 
 	loadProfile() {
@@ -41,6 +46,12 @@ export class ProfileComponent implements OnInit {
 			});
 		}
 	}
+
+	sortFunc(a: any, b: any): any {
+		return b.animalDate - a.animalDate
+	}
+
+
 
 	getJwtProfileId() : any {
 		if(this.authService.decodeJwt()) {
@@ -69,5 +80,9 @@ export class ProfileComponent implements OnInit {
 			return true;
 		}});
 		return false;
+	}
+
+	getAnimalbyProfileId(){
+		this.animalService.getAnimalbyProfileId(this.profileId).subscribe(reply => this.animals = reply);
 	}
 }
